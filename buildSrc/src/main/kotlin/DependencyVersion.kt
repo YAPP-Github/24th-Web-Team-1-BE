7fcfd72cc8b66d738688d76f62f4628f2ded897a
belljun3395 <195850@jnu.ac.kr>
2024-06-07 22:54:27 +0900

    feat: DependencyVersion 추가

---
+++ buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,30 @@
+object DependencyVersion {
+    /** kotlinVersion */
+    const val KOTLIN = "1.9.24"
+
+    /** ktlint */
+    const val KTLINT = "11.6.1"
+
+    /** springBootVersion */
+    const val SPRING_BOOT = "3.2.5"
+
+    /** springDependencyManagementVersion */
+    const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.5"
+
+    /** log */
+    const val LOGBACK_ENCODER = "7.4"
+
+    /** test */
+    const val MOCKK = "1.13.9"
+    const val KOTEST = "5.8.0"
+    const val KOTEST_EXTENSION = "1.1.3"
+    const val COROUTINE_TEST = "1.8.0"
+
+    /** docs */
+    const val ASCIIDOCTOR = "3.3.2"
+    const val EPAGES_REST_DOCS_API_SPEC = "0.16.0"
+
+    /** swagger */
+    const val SWAGGER_GENERATOR = "2.18.2"
+    const val SWAGGER_UI = "4.11.1"
+}

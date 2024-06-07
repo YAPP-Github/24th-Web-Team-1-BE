b0d9c91348b8165731afbcfdde9eefd1f7777fe1
belljun3395 <195850@jnu.ac.kr>
2024-06-08 01:27:50 +0900

    feat: data 모듈 flyway 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -10,5 +10,9 @@ dependencies {
     /** spring starter */
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("com.mysql:mysql-connector-j")
+
+    /** flyway */
+    implementation("org.flywaydb:flyway-core:${DependencyVersion.FLYWAY}")
+    implementation("org.flywaydb:flyway-mysql")
 }
 

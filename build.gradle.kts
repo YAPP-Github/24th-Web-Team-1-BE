19523c56aa039fe5972e700bf32a430feb0d2486
belljun3395 <195850@jnu.ac.kr>
2024-06-07 22:58:07 +0900

    feat: 프로젝트 root gradle 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,109 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version DependencyVersion.KOTLIN
+    kotlin("plugin.spring") version DependencyVersion.KOTLIN
+    kotlin("plugin.allopen") version DependencyVersion.KOTLIN
+    kotlin("kapt") version DependencyVersion.KOTLIN
+
+    /** spring */
+    id("org.springframework.boot") version DependencyVersion.SPRING_BOOT
+    id("io.spring.dependency-management") version DependencyVersion.SPRING_DEPENDENCY_MANAGEMENT
+
+    /** ktlint */
+    id("org.jlleitschuh.gradle.ktlint") version DependencyVersion.KTLINT
+
+    /** docs */
+    id("org.asciidoctor.jvm.convert") version DependencyVersion.ASCIIDOCTOR
+    id("com.epages.restdocs-api-spec") version DependencyVersion.EPAGES_REST_DOCS_API_SPEC
+    id("org.hidetake.swagger.generator") version DependencyVersion.SWAGGER_GENERATOR
 }
 
-group = "com.few"
-version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+allprojects {
+    group = "com.few"
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+
+    tasks.withType<Wrapper> {
+        gradleVersion = "8.5"
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
     }
 }
 
-repositories {
-    mavenCentral()
+tasks.getByName("bootJar") {
+    enabled = false
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+subprojects {
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.hidetake.swagger.generator")
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+    /**
+     * https://kotlinlang.org/docs/reference/compiler-plugins.html#spring-support
+     * automatically supported annotation
+     * @Component, @Async, @Transactional, @Cacheable, @SpringBootTest,
+     * @Configuration, @Controller, @RestController, @Service, @Repository.
+     * jpa meta-annotations not automatically opened through the default settings of the plugin.spring
+     */
+    allOpen {
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    dependencies {
+        /** spring starter */
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+        kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+        /** kotlin */
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        /** logger */
+        implementation("net.logstash.logback:logstash-logback-encoder:${DependencyVersion.LOGBACK_ENCODER}")
+
+        /** test **/
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.mockk:mockk:${DependencyVersion.MOCKK}")
+
+        /** kotest */
+        testImplementation("io.kotest:kotest-runner-junit5:${DependencyVersion.KOTEST}")
+        testImplementation("io.kotest:kotest-assertions-core:${DependencyVersion.KOTEST}")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:${DependencyVersion.KOTEST_EXTENSION}")
+        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${DependencyVersion.COROUTINE_TEST}")
+
+        /** swagger */
+        swaggerUI("org.webjars:swagger-ui:${DependencyVersion.SWAGGER_UI}")
+    }
+
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName("jar") {
+        enabled = true
+    }
+
+    defaultTasks("bootRun")
 }

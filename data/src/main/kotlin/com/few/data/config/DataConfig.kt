1cf70cfd4986669823ce3feddd95a5d04379386c
belljun3395 <195850@jnu.ac.kr>
2024-06-08 01:34:09 +0900

    feat: DataSource 빈 추가

--- data/src/main/kotlin/com/few/data/config/DataConfig.kt
+++ data/src/main/kotlin/com/few/data/config/DataConfig.kt
@@ -1,7 +1,14 @@
 package com.few.data.config
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.transaction.PlatformTransactionManager
+import javax.sql.DataSource
 
 @Configuration
 @ComponentScan(basePackages = [DataConfig.BASE_PACKAGE])
@@ -12,5 +19,12 @@ class DataConfig {
         const val MODULE_NAME = "few-data"
         const val BEAN_NAME_PREFIX = "fewData"
         const val PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME
+    }
+
+    @Primary
+    @Bean(name = [DataConfig.BEAN_NAME_PREFIX + "DataSource"])
+    @ConfigurationProperties(prefix = "spring.datasource")
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create().build()
     }
 }

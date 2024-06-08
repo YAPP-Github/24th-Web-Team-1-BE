d732db7a6e013da0069b9fbf4b6fbf7e536b5302
belljun3395 <195850@jnu.ac.kr>
2024-06-09 03:10:18 +0900

    feat: api-repo 모듈 jdbc, flyway 의존성 추가

--- api-repo/build.gradle.kts
+++ api-repo/build.gradle.kts
@@ -5,3 +5,13 @@ tasks.getByName("bootJar") {
 tasks.getByName("jar") {
     enabled = true
 }
+
+dependencies {
+    /** spring starter */
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("com.mysql:mysql-connector-j")
+
+    /** flyway */
+    implementation("org.flywaydb:flyway-core:${DependencyVersion.FLYWAY}")
+    implementation("org.flywaydb:flyway-mysql")
+}

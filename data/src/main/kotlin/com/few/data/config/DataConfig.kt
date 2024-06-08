3e66504b110b09b5c6832ff1da0b884da12ba84f
belljun3395 <195850@jnu.ac.kr>
2024-06-08 19:40:59 +0900

    remove: 데이터 소스 관련 코드 삭제

--- data/src/main/kotlin/com/few/data/config/DataConfig.kt
+++ data/src/main/kotlin/com/few/data/config/DataConfig.kt
@@ -1,14 +1,7 @@
 package com.few.data.config
 
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.jdbc.DataSourceBuilder
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.jdbc.datasource.DataSourceTransactionManager
-import org.springframework.transaction.PlatformTransactionManager
-import javax.sql.DataSource
 
 @Configuration
 @ComponentScan(basePackages = [DataConfig.BASE_PACKAGE])
@@ -19,17 +12,5 @@ class DataConfig {
         const val MODULE_NAME = "few-data"
         const val BEAN_NAME_PREFIX = "fewData"
         const val PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME
-    }
-
-    @Primary
-    @Bean(name = [DataConfig.BEAN_NAME_PREFIX + "DataSource"])
-    @ConfigurationProperties(prefix = "spring.datasource")
-    fun dataSource(): DataSource {
-        return DataSourceBuilder.create().build()
-    }
-
-    @Bean(name = [DataConfig.BEAN_NAME_PREFIX + "TransactionManager"])
-    fun transactionManager(dataSource: DataSource): PlatformTransactionManager {
-        return DataSourceTransactionManager(dataSource)
     }
 }

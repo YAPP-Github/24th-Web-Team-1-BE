4ee56d94035f7355c2b376b7fbc9d176d1d3a97a
belljun3395 <195850@jnu.ac.kr>
2024-06-10 12:56:29 +0900

    feat: RepoTestContainerInitializer 적용

--- api-repo/src/test/kotlin/com/few/api/repo/jooq/JooqTestSpec.kt
+++ api-repo/src/test/kotlin/com/few/api/repo/jooq/JooqTestSpec.kt
@@ -1,9 +1,12 @@
 package com.few.api.repo.jooq
 
+import com.few.api.repo.RepoTestContainerInitializer
 import com.few.api.repo.config.ApiRepoConfig
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 
 @ActiveProfiles("new", "test", "api-repo-local")
 @SpringBootTest(classes = [ApiRepoConfig::class])
+@ContextConfiguration(initializers = [RepoTestContainerInitializer::class])
 abstract class JooqTestSpec

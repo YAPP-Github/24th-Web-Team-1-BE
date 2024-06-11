60a1695960c9904eaf542820a8b6ab713c9c7a2c
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-06-11 23:56:31 +0900

    fix: DDL 리뷰 반영

--- data/src/main/resources/db/migration/entity/V1.00.0.1__draft_table_design.sql
+++ data/src/main/resources/db/migration/entity/V1.00.0.1__draft_table_design.sql
@@ -1,11 +1,13 @@
 -- 작가 및 유저
-CREATE TABLE users
+CREATE TABLE member
 (
-    id         BIGINT       NOT NULL AUTO_INCREMENT,
-    email      VARCHAR(255) NOT NULL,
-    type_cd    TINYINT      NOT NULL,
-    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL DEFAULT NULL,
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    email       VARCHAR(255) NOT NULL,
+    type_cd     TINYINT      NOT NULL,
+    description JSON         NOT NULL DEFAULT (JSON_OBJECT()),
+    created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at  TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
     UNIQUE (email)
 );
@@ -14,7 +16,7 @@ CREATE TABLE users
 CREATE TABLE article_mst
 (
     id             BIGINT       NOT NULL AUTO_INCREMENT,
-    user_id        BIGINT       NOT NULL,
+    member_id      BIGINT       NOT NULL,
     main_image_url VARCHAR(255) NOT NULL,
     title          VARCHAR(255) NOT NULL,
     category_cd    TINYINT      NOT NULL,
@@ -46,26 +48,22 @@ CREATE TABLE workbook
 );
 
 -- 작가-학습지 매핑테이블(다대다)
-CREATE TABLE mapping_user_workbook
+CREATE TABLE mapping_member_workbook
 (
-    id          BIGINT NOT NULL AUTO_INCREMENT,
-    user_id     BIGINT NOT NULL,
+    member_id   BIGINT NOT NULL,
     workbook_id BIGINT NOT NULL,
     deleted_at  TIMESTAMP NULL DEFAULT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (workbook_id, user_id)
+    PRIMARY KEY (workbook_id, member_id)
 );
 
 -- 학습지-아티클 매핑테이블(다대다)
 CREATE TABLE mapping_workbook_article
 (
-    id          BIGINT NOT NULL AUTO_INCREMENT,
     workbook_id BIGINT NOT NULL,
     article_id  BIGINT NOT NULL,
     day_col     int    NOT NULL,
     deleted_at  TIMESTAMP NULL DEFAULT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (article_id, workbook_id)
+    PRIMARY KEY (workbook_id, article_id)
 );
 
 -- 문제, 정답, 해설(메타데이터)
@@ -74,7 +72,7 @@ CREATE TABLE problem
     id          BIGINT       NOT NULL AUTO_INCREMENT,
     article_id  BIGINT       NOT NULL,
     title       VARCHAR(255) NOT NULL,
-    content     JSON         NOT NULL,
+    contents    JSON         NOT NULL,
     answer      VARCHAR(255) NOT NULL,
     explanation VARCHAR(255) NOT NULL,
     creator_id  BIGINT       NOT NULL,
@@ -84,14 +82,14 @@ CREATE TABLE problem
 );
 
 -- 풀이 히스토리
-CREATE TABLE solve_history
+CREATE TABLE submit_history
 (
-    id         BIGINT    NOT NULL AUTO_INCREMENT,
-    problem_id BIGINT    NOT NULL,
-    user_id    BIGINT    NOT NULL,
-    choice_ans TINYINT   NOT NULL,
-    is_solved  bit(1)    NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    problem_id BIGINT       NOT NULL,
+    member_id  BIGINT       NOT NULL,
+    submit_ans VARCHAR(255) NOT NULL,
+    is_solved  bit(1)       NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id)
 );
@@ -100,30 +98,19 @@ CREATE TABLE solve_history
 CREATE TABLE subscription
 (
     id                 BIGINT    NOT NULL AUTO_INCREMENT,
-    user_id            BIGINT    NOT NULL,
-    target_user_id     BIGINT    NOT NULL,
-    target_workbook_id BIGINT    NOT NULL,
+    member_id          BIGINT    NOT NULL,
+    target_member_id   BIGINT NULL,
+    target_workbook_id BIGINT NULL,
     created_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at         TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
-    UNIQUE (user_id, target_user_id),
-    UNIQUE (user_id, target_workbook_id)
+    UNIQUE (target_member_id, member_id),
+    UNIQUE (target_workbook_id, member_id)
 );
 
 -- [인덱스 추가] --
--- 공통 인덱스: deleted_at 인덱스
-CREATE INDEX users_idx0 ON users (deleted_at)
-CREATE INDEX article_mst_idx0 ON article_mst (deleted_at)
-CREATE INDEX article_ifo_idx0 ON article_ifo (deleted_at)
-CREATE INDEX workbook_idx0 ON workbook (deleted_at)
-CREATE INDEX mapping_user_workbook_idx0 ON mapping_user_workbook (deleted_at)
-CREATE INDEX mapping_workbook_article_idx0 ON mapping_workbook_article (deleted_at)
-CREATE INDEX problem_idx0 ON problem (deleted_at)
-CREATE INDEX solve_history_idx0 ON solve_history (deleted_at)
-CREATE INDEX subscription_idx0 ON subscription (deleted_at)
-
 -- problem_idx1: problem 테이블에서 article_id 기반으로 문제 조회시 사용
 CREATE INDEX problem_idx1 ON problem (article_id)
 
 -- article_mst_idx1: 작가가 작성한 아티클 조회시 사용
-CREATE INDEX article_mst_idx1 ON article_mst (user_id)
+CREATE INDEX article_mst_idx1 ON article_mst (member_id)

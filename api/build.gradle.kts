d816be1fcab92d2bed7c960c52bc3531fafe7507
belljun3395 <195850@jnu.ac.kr>
2024-06-13 13:17:14 +0900

    feat: openapi3, postman 생성 코드를 위한 테스크 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,3 +8,29 @@ dependencies {
     implementation("org.springframework.restdocs:spring-restdocs-webtestclient")
     implementation("com.epages:restdocs-api-spec-mockmvc:${DependencyVersion.EPAGES_REST_DOCS_API_SPEC}")
 }
+
+plugins {
+    id("org.asciidoctor.jvm.convert") version DependencyVersion.ASCIIDOCTOR
+    id("com.epages.restdocs-api-spec") version DependencyVersion.EPAGES_REST_DOCS_API_SPEC
+    id("org.hidetake.swagger.generator") version DependencyVersion.SWAGGER_GENERATOR
+}
+
+/** convert snippet to swagger */
+openapi3 {
+    this.setServer("http://localhost:8080") // todo refactor to use setServers
+    title = project.name
+    version = project.version.toString()
+    format = "yaml"
+    snippetsDirectory = "build/generated-snippets/"
+    outputDirectory = "src/main/resources/static/"
+    outputFileNamePrefix = "openapi3"
+}
+
+/** convert snippet to postman */
+postman {
+    title = project.name
+    version = project.version.toString()
+    baseUrl = "http://localhost:8080"
+    outputDirectory = "src/main/resources/static/"
+    outputFileNamePrefix = "postman"
+}

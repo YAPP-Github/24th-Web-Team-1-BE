c2d30bb572627f86d1350736214b1a1ba4cda407
belljun3395 <195850@jnu.ac.kr>
2024-06-13 13:16:28 +0900

    feat: swagger & restdocs 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,4 +2,9 @@ dependencies {
     /** spring starter */
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    /** swagger & restdocs */
+    implementation("org.springdoc:springdoc-openapi-ui:${DependencyVersion.SPRINGDOC}")
+    implementation("org.springframework.restdocs:spring-restdocs-webtestclient")
+    implementation("com.epages:restdocs-api-spec-mockmvc:${DependencyVersion.EPAGES_REST_DOCS_API_SPEC}")
 }

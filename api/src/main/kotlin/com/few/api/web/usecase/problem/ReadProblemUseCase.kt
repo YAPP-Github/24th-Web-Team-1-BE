33c69bcc9398e4aade3a113909e6227478bbaaa5
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-06-19 22:38:21 +0900

    fix: 문제 컨텐츠(json string) to Object 변환 과정 추가

--- api/src/main/kotlin/com/few/api/web/usecase/problem/ReadProblemUseCase.kt
+++ api/src/main/kotlin/com/few/api/web/usecase/problem/ReadProblemUseCase.kt
@@ -1,26 +1,33 @@
 package com.few.api.web.usecase.problem
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.few.api.repo.dao.problem.ProblemDao
 import com.few.api.repo.dao.problem.query.SelectProblemQuery
 import com.few.api.web.usecase.problem.`in`.ReadProblemUseCaseIn
+import com.few.api.web.usecase.problem.out.ReadProblemContentsUseCaseOut
 import com.few.api.web.usecase.problem.out.ReadProblemUseCaseOut
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class ReadProblemUseCase(
-    private val prblemDao: ProblemDao
+    private val prblemDao: ProblemDao,
+    private val objectMapper: ObjectMapper
 ) {
 
     @Transactional(readOnly = true)
     fun execute(useCaseIn: ReadProblemUseCaseIn): ReadProblemUseCaseOut {
         val problemId = useCaseIn.problemId
 
-        val record = ProblemDao.selectProblem(SelectProblemQuery(problemId))
+        val record = prblemDao.selectProblem(SelectProblemQuery(problemId))
+
+        val contents: List<ReadProblemContentsUseCaseOut> = objectMapper.readValue(record.contents)
 
         return ReadProblemUseCaseOut(
             id = record.id,
             title = record.title,
-            contents = record.contents // TODO: 리스트 변환
+            contents = contents
         )
     }
 }

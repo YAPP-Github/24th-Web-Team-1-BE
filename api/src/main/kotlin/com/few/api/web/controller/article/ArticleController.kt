8f584ce991d6fb94c8b21ba1564efc59dcc0899d
belljun3395 <195850@jnu.ac.kr>
2024-06-21 02:36:09 +0900

    refactor: ReadArticleUseCase 컨트롤러에 적용

--- api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
+++ api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
@@ -1,5 +1,7 @@
 package com.few.api.web.controller.article
 
+import com.few.api.domain.article.dto.ReadArticleUseCaseIn
+import com.few.api.domain.article.usecase.ReadArticleUseCase
 import com.few.api.web.controller.article.response.ReadArticleResponse
 import com.few.api.web.controller.article.response.WriterInfo
 import com.few.api.web.support.ApiResponse
@@ -10,31 +12,35 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.net.URL
-import java.time.LocalDateTime
 
 @Validated
 @RestController
 @RequestMapping("/api/v1/articles")
-class ArticleController {
+class ArticleController(
+    private val readArticleUseCase: ReadArticleUseCase
+) {
 
     @GetMapping("/{articleId}")
     fun readArticle(
         @PathVariable(value = "articleId") articleId: Long
     ): ApiResponse<ApiResponse.SuccessBody<ReadArticleResponse>> {
-        val data = ReadArticleResponse(
-            id = 1L,
+        val useCaseOut = ReadArticleUseCaseIn(articleId).let { useCaseIn: ReadArticleUseCaseIn ->
+            readArticleUseCase.execute(useCaseIn)
+        }
+
+        val response = ReadArticleResponse(
+            id = useCaseOut.id,
             writer = WriterInfo(
-                id = 1L,
-                name = "안나포",
-                url = URL("http://localhost:8080/api/v1/writers/1")
+                id = useCaseOut.writer.id,
+                name = useCaseOut.writer.name,
+                url = useCaseOut.writer.url
             ),
-            title = "ETF(상장 지수 펀드)란? 모르면 손해라고?",
-            content = "content",
-            problemIds = listOf(1L, 2L, 3L),
-            category = "경제",
-            createdAt = LocalDateTime.now()
+            title = useCaseOut.title,
+            content = useCaseOut.content,
+            problemIds = useCaseOut.problemIds,
+            category = useCaseOut.category,
+            createdAt = useCaseOut.createdAt
         )
-        return ApiResponseGenerator.success(data, HttpStatus.OK)
+        return ApiResponseGenerator.success(response, HttpStatus.OK)
     }
 }

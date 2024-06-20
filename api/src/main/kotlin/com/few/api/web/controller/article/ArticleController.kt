e69718088243e58690c6e636bf0580b1b8f517e1
belljun3395 <195850@jnu.ac.kr>
2024-06-21 02:43:55 +0900

    refactor: ReadArticleResponse 응답 구현 수정

--- api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
+++ api/src/main/kotlin/com/few/api/web/controller/article/ArticleController.kt
@@ -3,7 +3,6 @@ package com.few.api.web.controller.article
 import com.few.api.domain.article.dto.ReadArticleUseCaseIn
 import com.few.api.domain.article.usecase.ReadArticleUseCase
 import com.few.api.web.controller.article.response.ReadArticleResponse
-import com.few.api.web.controller.article.response.WriterInfo
 import com.few.api.web.support.ApiResponse
 import com.few.api.web.support.ApiResponseGenerator
 import org.springframework.http.HttpStatus
@@ -28,19 +27,8 @@ class ArticleController(
             readArticleUseCase.execute(useCaseIn)
         }
 
-        val response = ReadArticleResponse(
-            id = useCaseOut.id,
-            writer = WriterInfo(
-                id = useCaseOut.writer.id,
-                name = useCaseOut.writer.name,
-                url = useCaseOut.writer.url
-            ),
-            title = useCaseOut.title,
-            content = useCaseOut.content,
-            problemIds = useCaseOut.problemIds,
-            category = useCaseOut.category,
-            createdAt = useCaseOut.createdAt
-        )
-        return ApiResponseGenerator.success(response, HttpStatus.OK)
+        return ReadArticleResponse(useCaseOut).let {
+            ApiResponseGenerator.success(it, HttpStatus.OK)
+        }
     }
 }

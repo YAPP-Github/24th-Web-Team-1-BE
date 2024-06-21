4efcfb316d0a0d82cf552a3b07f42b96a75b4c00
belljun3395 <195850@jnu.ac.kr>
2024-06-21 11:11:47 +0900

    refactor: ReadWorkBookArticleUseCase 컨트롤러에 적용

--- api/src/main/kotlin/com/few/api/web/controller/workbook/article/response/ReadWorkBookArticleResponse.kt
+++ api/src/main/kotlin/com/few/api/web/controller/workbook/article/response/ReadWorkBookArticleResponse.kt
@@ -1,5 +1,6 @@
 package com.few.api.web.controller.workbook.article.response
 
+import com.few.api.domain.workbook.article.dto.ReadWorkBookArticleOut
 import com.few.api.web.controller.workbook.response.WriterInfo
 import java.time.LocalDateTime
 
@@ -12,4 +13,19 @@ data class ReadWorkBookArticleResponse(
     val category: String,
     val createdAt: LocalDateTime,
     val day: Long
-)
+) {
+    constructor(useCaseOut: ReadWorkBookArticleOut) : this(
+        id = useCaseOut.id,
+        writer = WriterInfo(
+            id = useCaseOut.writer.id,
+            name = useCaseOut.writer.name,
+            url = useCaseOut.writer.url
+        ),
+        title = useCaseOut.title,
+        content = useCaseOut.content,
+        problemIds = useCaseOut.problemIds,
+        category = useCaseOut.category,
+        createdAt = useCaseOut.createdAt,
+        day = useCaseOut.day
+    )
+}

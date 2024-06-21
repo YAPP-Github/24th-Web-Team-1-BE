a2a8bb77f27156b730cd4f6c06e2fc9fd9250af9
belljun3395 <195850@jnu.ac.kr>
2024-06-21 11:06:41 +0900

    test: selectWorkBookArticleRecord 테스트 구현

--- api-repo/src/test/kotlin/com/few/api/repo/dao/article/ArticleDaoTest.kt
+++ api-repo/src/test/kotlin/com/few/api/repo/dao/article/ArticleDaoTest.kt
@@ -1,9 +1,11 @@
 package com.few.api.repo.dao.article
 
 import com.few.api.repo.dao.article.query.SelectArticleRecordQuery
+import com.few.api.repo.dao.article.query.SelectWorkBookArticleRecordQuery
 import com.few.api.repo.jooq.JooqTestSpec
 import jooq.jooq_dsl.tables.ArticleIfo
 import jooq.jooq_dsl.tables.ArticleMst
+import jooq.jooq_dsl.tables.MappingWorkbookArticle
 import org.jooq.DSLContext
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
@@ -60,5 +62,45 @@ class ArticleDaoTest : JooqTestSpec() {
         assertEquals("this is title", result.title)
         assertEquals("this is content", result.content)
         assertEquals(0, result.category)
+    }
+
+    @Test
+    @Transactional
+    fun `학습지 Id와 아티클 Id를 통해 학습지에서의 아티클 Day가 포함된 아티클 정보를 조회합니다`() {
+        // given
+        setMappingWorkbookArticleData()
+        val query = SelectWorkBookArticleRecordQuery(1L, 1L)
+
+        // when
+        val result = query.let {
+            articleDao.selectWorkBookArticleRecord(it)
+        }
+
+        // then
+        assertNotNull(result)
+        assertEquals(1L, result.articleId)
+        assertEquals(1L, result.writerId)
+        assertEquals("http://localhost:8080/image.jpg", result.mainImageURL)
+        assertEquals("this is title", result.title)
+        assertEquals("this is content", result.content)
+        assertEquals(0, result.category)
+        assertEquals(1L, result.day)
+    }
+
+    private fun setMappingWorkbookArticleData() {
+        log.debug("===== start setMappingWorkbookArticleData =====")
+        dslContext.deleteFrom(MappingWorkbookArticle.MAPPING_WORKBOOK_ARTICLE).execute()
+        setMappingWorkbookArticle(1L, 1L, 1)
+        setMappingWorkbookArticle(1L, 2L, 2)
+        setMappingWorkbookArticle(1L, 3L, 3)
+        log.debug("===== finish setMappingWorkbookArticleData =====")
+    }
+
+    private fun setMappingWorkbookArticle(workbookId: Long, articleId: Long, day: Int) {
+        dslContext.insertInto(MappingWorkbookArticle.MAPPING_WORKBOOK_ARTICLE)
+            .set(MappingWorkbookArticle.MAPPING_WORKBOOK_ARTICLE.WORKBOOK_ID, workbookId)
+            .set(MappingWorkbookArticle.MAPPING_WORKBOOK_ARTICLE.ARTICLE_ID, articleId)
+            .set(MappingWorkbookArticle.MAPPING_WORKBOOK_ARTICLE.DAY_COL, day)
+            .execute()
     }
 }

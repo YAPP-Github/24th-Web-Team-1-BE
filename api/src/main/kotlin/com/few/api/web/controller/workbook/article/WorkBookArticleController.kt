be09bd6c14b525f391de14f56f277b794816d11e
belljun3395 <195850@jnu.ac.kr>
2024-06-22 22:08:32 +0900

    feat: GET /api/v1/workbooks/{workbookId}/articles/{articleId} 구현 (#65)
    * refactor: ApiMain 클래스 이동
    
    * feat: api-repo 모듈 ObjectMapper 빈 선언
    
    * feat: selectArticleRecord 메서드 구현
    
    * test: selectArticleRecord 테스트 구현
    
    * feat: selectWriter 메서드 구현
    
    * test: selectWriter 테스트 구현
    
    * feat: WriterDescription 구현
    
    * test: WriterDescriptionMapperTest 테스트 구현
    
    * feat: selectProblemsByArticleId 메서드 구현
    
    * test: selectProblemsByArticleId 테스트 구현
    
    * feat: Contents 구현
    
    * test: ContentsMapperTest 테스트 구현
    
    * feat: BrowseArticleProblemsService 구현
    
    * feat: ReadWriterRecordService 구현
    
    * feat: ReadArticleUseCase 구현
    
    * feat: ApiRepoObjectMapperConfig 삭제
    
    * refactor: init 블록에서 registerKotlinModule 수행되도록 수정
    
    * refactor: 기본 등록 ObjectMapper 사용하도록 수정
    
    * refactor: ReadArticleUseCase 컨트롤러에 적용
    
    * refactor: readArticle 컨트롤러 테스트 유스케이스 목 처리
    
    * refactor: ReadArticleResponse 응답 구현 수정
    
    * refactor: 쿼리 객체에 Id 특성을 추가하여 추후 Problems 조회도 가능할 수 있도록 수정
    
    * refactor: 아티클 작성자를 조회하는 것임을 명확히 하기 위해 수정
    
    * chore: 해결된 Todo 삭제
    
    * feat: selectWorkBookArticleRecord 메서드 구현
    
    * test: selectWorkBookArticleRecord 테스트 구현
    
    * feat: ReadWorkBookArticleUseCase 구현
    
    * refactor: ReadWorkBookArticleUseCase 컨트롤러에 적용
    
    * refactor: readWorkBookArticle 컨트롤러에 테스트 유스케이스 목 처리

--- api/src/main/kotlin/com/few/api/web/controller/workbook/article/WorkBookArticleController.kt
+++ api/src/main/kotlin/com/few/api/web/controller/workbook/article/WorkBookArticleController.kt
@@ -1,7 +1,8 @@
 package com.few.api.web.controller.workbook.article
 
+import com.few.api.domain.workbook.article.dto.ReadWorkBookArticleUseCaseIn
+import com.few.api.domain.workbook.article.usecase.ReadWorkBookArticleUseCase
 import com.few.api.web.controller.workbook.article.response.ReadWorkBookArticleResponse
-import com.few.api.web.controller.workbook.response.WriterInfo
 import com.few.api.web.support.ApiResponse
 import com.few.api.web.support.ApiResponseGenerator
 import org.springframework.http.HttpStatus
@@ -10,33 +11,25 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.net.URL
-import java.time.LocalDateTime
 
 @Validated
 @RestController
 @RequestMapping("/api/v1/workbooks/{workbookId}/articles")
-class WorkBookArticleController {
+class WorkBookArticleController(
+    private val readWorkBookArticleUseCase: ReadWorkBookArticleUseCase
+) {
 
     @GetMapping("/{articleId}")
     fun readWorkBookArticle(
         @PathVariable(value = "workbookId") workbookId: Long,
         @PathVariable(value = "articleId") articleId: Long
     ): ApiResponse<ApiResponse.SuccessBody<ReadWorkBookArticleResponse>> {
-        val data = ReadWorkBookArticleResponse(
-            id = 1L,
-            writer = WriterInfo(
-                id = 1L,
-                name = "안나포",
-                url = URL("http://localhost:8080/api/v1/writers/1")
-            ),
-            title = "ETF(상장 지수 펀드)란? 모르면 손해라고?",
-            content = "content",
-            problemIds = listOf(1L, 2L, 3L),
-            category = "경제",
-            createdAt = LocalDateTime.now(),
-            day = 1L
-        )
-        return ApiResponseGenerator.success(data, HttpStatus.OK)
+        val useCaseOut = ReadWorkBookArticleUseCaseIn(workbookId = workbookId, articleId = articleId).let { useCaseIn: ReadWorkBookArticleUseCaseIn ->
+            readWorkBookArticleUseCase.execute(useCaseIn)
+        }
+
+        return ReadWorkBookArticleResponse(useCaseOut).let {
+            ApiResponseGenerator.success(it, HttpStatus.OK)
+        }
     }
 }

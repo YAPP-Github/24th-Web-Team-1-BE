612675ff1c7eb5edeb0b47541664cbc53a2ef873
belljun3395 <195850@jnu.ac.kr>
2024-06-22 19:21:52 +0900

    refactor: 테스트에서 MailProperties를 통해 자동으로 설정 값이 매핑되지 않아 수정

--- email/src/main/kotlin/com/few/email/config/MailSenderConfig.kt
+++ email/src/main/kotlin/com/few/email/config/MailSenderConfig.kt
@@ -1,10 +1,10 @@
 package com.few.email.config
 
 import com.few.email.config.MailConfig.Companion.BEAN_NAME_PREFIX
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.mail.MailProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.JavaMailSenderImpl
 import java.util.*
@@ -22,10 +22,46 @@ class MailSenderConfig {
         const val MAIL_SMTP_ENABLE_SSL_ENABLE_KEY = "mail.smtp.EnableSSL.enable"
     }
 
-    @Primary
+    @Value("\${spring.mail.protocol}")
+    private lateinit var protocol: String
+
+    @Value("\${spring.mail.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.mail.port}")
+    private lateinit var port: String
+
+    @Value("\${spring.mail.username}")
+    private lateinit var username: String
+
+    @Value("\${spring.mail.password}")
+    private lateinit var password: String
+
+    @Value("\${spring.mail.properties.mail.smtp.auth}")
+    private lateinit var auth: String
+
+    @Value("\${spring.mail.properties.mail.smtp.debug}")
+    private lateinit var debug: String
+
+    @Value("\${spring.mail.properties.mail.smtp.starttls.enable}")
+    private lateinit var starttls: String
+
+    @Value("\${spring.mail.properties.mail.smtp.EnableSSL.enable}")
+    private lateinit var enableSSL: String
+
     @Bean(name = [MAIL_PROPERTIES])
     fun mailProperties(): MailProperties {
-        return MailProperties()
+        val mailProperties = MailProperties()
+        mailProperties.protocol = protocol
+        mailProperties.host = host
+        mailProperties.port = port.toInt()
+        mailProperties.username = username
+        mailProperties.password = password
+        mailProperties.properties[MAIL_SMTP_AUTH_KEY] = auth
+        mailProperties.properties[MAIL_SMTP_DEBUG_KEY] = debug
+        mailProperties.properties[MAIL_SMTP_STARTTLS_ENABLE_KEY] = starttls
+        mailProperties.properties[MAIL_SMTP_ENABLE_SSL_ENABLE_KEY] = enableSSL
+        return mailProperties
     }
 
     @Bean(name = [MAIL_SENDER])

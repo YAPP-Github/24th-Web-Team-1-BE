dab223c2f40321664c92d4013ff3617f30c6b860
belljun3395 <195850@jnu.ac.kr>
2024-06-23 14:32:38 +0900

    feat: BatchCallExecutionService 구현

---
+++ batch/src/main/kotlin/com/few/batch/log/BatchCallExecutionService.kt
@@ -0,0 +1,18 @@
+package com.few.batch.log
+
+import jooq.jooq_dsl.Tables.BATCH_CALL_EXECUTION
+import org.jooq.DSLContext
+import org.jooq.JSON
+import org.springframework.stereotype.Service
+
+@Service
+class BatchCallExecutionService(
+    private val dslContext: DSLContext
+) {
+    fun execute(status: Boolean, jsonDescription: String) {
+        dslContext.insertInto(BATCH_CALL_EXECUTION)
+            .set(BATCH_CALL_EXECUTION.STATUS, status)
+            .set(BATCH_CALL_EXECUTION.DESCRIPTION, JSON.valueOf(jsonDescription))
+            .execute()
+    }
+}

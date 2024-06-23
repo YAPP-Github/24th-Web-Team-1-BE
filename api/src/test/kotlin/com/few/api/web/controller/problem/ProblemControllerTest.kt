89655f3d0891789c5abb371f32c90a0bc978d50d
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-06-23 12:09:05 +0900

    test: 모킹 인풋 객체 추가

--- api/src/test/kotlin/com/few/api/web/controller/problem/ProblemControllerTest.kt
+++ api/src/test/kotlin/com/few/api/web/controller/problem/ProblemControllerTest.kt
@@ -11,13 +11,14 @@ import com.few.api.web.controller.helper.*
 import com.few.api.web.controller.problem.request.CheckProblemBody
 import com.few.api.web.usecase.problem.CheckProblemUseCase
 import com.few.api.web.usecase.problem.ReadProblemUseCase
+import com.few.api.web.usecase.problem.`in`.CheckProblemUseCaseIn
+import com.few.api.web.usecase.problem.`in`.ReadProblemUseCaseIn
 import com.few.api.web.usecase.problem.out.CheckProblemUseCaseOut
 import com.few.api.web.usecase.problem.out.ReadProblemContentsUseCaseOutDetail
 import com.few.api.web.usecase.problem.out.ReadProblemUseCaseOut
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -70,8 +71,10 @@ class ProblemControllerTest : ControllerTestSpec() {
             .toUriString()
 
         // set usecase mock
+        val problemId = 1L
+        val useCaseIn = ReadProblemUseCaseIn(problemId)
         val useCaseOut = ReadProblemUseCaseOut(
-            id = 1L,
+            id = problemId,
             title = "ETF(상장지수펀드)의 특징이 아닌것은?",
             contents = listOf(
                 ReadProblemContentsUseCaseOutDetail(1L, "분산투자"),
@@ -80,7 +83,7 @@ class ProblemControllerTest : ControllerTestSpec() {
                 ReadProblemContentsUseCaseOutDetail(4L, "투명성")
             )
         )
-        Mockito.`when`(readProblemUseCase.execute(ArgumentMatchers.any()))
+        Mockito.`when`(readProblemUseCase.execute(useCaseIn))
             .thenReturn(useCaseOut)
 
         // when
@@ -136,12 +139,15 @@ class ProblemControllerTest : ControllerTestSpec() {
         val body = objectMapper.writeValueAsString(CheckProblemBody(sub = "sub"))
 
         // set usecase mock
+        val problemId = 1L
+        val sub = "제출답"
+        val useCaseIn = CheckProblemUseCaseIn(problemId, sub = sub)
         val useCaseOut = CheckProblemUseCaseOut(
             explanation = "ETF는 일반적으로 낮은 운용 비용을 특징으로 합니다.이는 ETF가 보통 지수 추종(passive management) 방식으로 운용되기 때문입니다. 지수를 추종하는 전략은 액티브 매니지먼트(active management)에 비해 관리가 덜 복잡하고, 따라서 비용이 낮습니다.",
             answer = "2",
             isSolved = true
         )
-        Mockito.`when`(checkProblemUseCase.execute(ArgumentMatchers.any()))
+        Mockito.`when`(checkProblemUseCase.execute(useCaseIn))
             .thenReturn(useCaseOut)
 
         // when

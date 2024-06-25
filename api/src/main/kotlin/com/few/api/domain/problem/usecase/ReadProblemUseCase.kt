0b6ce763b0ec786a96f2b09d9a07f900181a2f50
belljun3395 <195850@jnu.ac.kr>
2024-06-26 02:50:52 +0900

    refactor: contentsJsonMapper로 변환 구현하도록 수정

--- api/src/main/kotlin/com/few/api/domain/problem/usecase/ReadProblemUseCase.kt
+++ api/src/main/kotlin/com/few/api/domain/problem/usecase/ReadProblemUseCase.kt
@@ -1,19 +1,18 @@
 package com.few.api.domain.problem.usecase
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.few.api.repo.dao.problem.ProblemDao
 import com.few.api.repo.dao.problem.query.SelectProblemQuery
 import com.few.api.domain.problem.usecase.`in`.ReadProblemUseCaseIn
 import com.few.api.domain.problem.usecase.out.ReadProblemContentsUseCaseOutDetail
 import com.few.api.domain.problem.usecase.out.ReadProblemUseCaseOut
+import com.few.api.repo.dao.problem.support.ContentsJsonMapper
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
 class ReadProblemUseCase(
     private val problemDao: ProblemDao,
-    private val objectMapper: ObjectMapper
+    private val contentsJsonMapper: ContentsJsonMapper
 ) {
 
     @Transactional(readOnly = true)
@@ -22,7 +21,12 @@ class ReadProblemUseCase(
 
         val record = problemDao.selectProblemContents(SelectProblemQuery(problemId))
 
-        val contents: List<ReadProblemContentsUseCaseOutDetail> = objectMapper.readValue(record.contents)
+        val contents: List<ReadProblemContentsUseCaseOutDetail> = contentsJsonMapper.toObject(record.contents).contents.map {
+            ReadProblemContentsUseCaseOutDetail(
+                number = it.number,
+                content = it.content
+            )
+        }
 
         return ReadProblemUseCaseOut(
             id = record.id,

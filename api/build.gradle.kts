959a996a28258e3c6f14c8450243a498ca1adaf1
belljun3395 <195850@jnu.ac.kr>
2024-06-26 02:55:23 +0900

    refactor: openapi 의존성 webflux 버전으로 수정

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     /** swagger & restdocs */
-    implementation("org.springdoc:springdoc-openapi-ui:${DependencyVersion.SPRINGDOC}")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:${DependencyVersion.SPRINGDOC}")
     implementation("org.springframework.restdocs:spring-restdocs-webtestclient")
     implementation("com.epages:restdocs-api-spec-mockmvc:${DependencyVersion.EPAGES_REST_DOCS_API_SPEC}")
 

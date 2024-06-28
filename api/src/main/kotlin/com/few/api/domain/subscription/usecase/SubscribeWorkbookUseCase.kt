3ecf2b3303d576a6fb1c3034b700b14db52375cd
belljun3395 <195850@jnu.ac.kr>
2024-06-28 17:18:48 +0900

    refactor: 재구독 상황 고려 리펙토링

--- api/src/main/kotlin/com/few/api/domain/subscription/usecase/SubscribeWorkbookUseCase.kt
+++ api/src/main/kotlin/com/few/api/domain/subscription/usecase/SubscribeWorkbookUseCase.kt
@@ -5,8 +5,9 @@ import com.few.api.domain.subscription.service.dto.InsertMemberDto
 import com.few.api.domain.subscription.service.dto.ReadMemberIdDto
 import com.few.api.repo.dao.subscription.SubscriptionDao
 import com.few.api.repo.dao.subscription.command.InsertWorkbookSubscriptionCommand
-import com.few.api.repo.dao.subscription.query.CountWorkbookSubscriptionQuery
+import com.few.api.repo.dao.subscription.query.SelectAllWorkbookSubscriptionStatusQueryNotConsiderDeletedAt
 import com.few.api.domain.subscription.usecase.`in`.SubscribeWorkbookUseCaseIn
+import com.few.api.repo.dao.subscription.query.CountWorkbookMappedArticlesQuery
 import com.few.data.common.code.MemberType
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -25,17 +26,30 @@ class SubscribeWorkbookUseCase(
             InsertMemberDto(useCaseIn.email, MemberType.NORMAL)
         )
 
-        // 이미 구독중인지 확인
-        CountWorkbookSubscriptionQuery(memberId = memberId, workbookId = useCaseIn.workbookId).let { query ->
-            subscriptionDao.selectCountWorkbookSubscription(query).let { cnt ->
-                if (cnt > 0) {
-                    throw RuntimeException("Already subscribed")
+        val subTargetWorkbookId = useCaseIn.workbookId
+        SelectAllWorkbookSubscriptionStatusQueryNotConsiderDeletedAt(memberId = memberId, workbookId = subTargetWorkbookId).let { query ->
+            subscriptionDao.selectAllWorkbookSubscriptionStatus(query).let { subscriptionStatusList ->
+                if (subscriptionStatusList.isNotEmpty()) {
+                    subscriptionStatusList.stream().filter { status ->
+                        status.id == query.workbookId
+                    }.findAny().get().let { status ->
+                        InsertWorkbookSubscriptionCommand(memberId = memberId, workbookId = subTargetWorkbookId).let { command ->
+                            if (status.subHistory) {
+                                CountWorkbookMappedArticlesQuery(subTargetWorkbookId).let { query ->
+                                    subscriptionDao.countWorkbookMappedArticles(query)
+                                }?.let { lastDay ->
+                                    if (lastDay <= (status.day)) {
+                                        throw RuntimeException("이미 학습을 완료한 워크북입니다.")
+                                    }
+                                    subscriptionDao.reSubscribeWorkbookSubscription(command)
+                                }
+                            } else {
+                                subscriptionDao.insertWorkbookSubscription(command)
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        subscriptionDao.insertWorkbookSubscription(
-            InsertWorkbookSubscriptionCommand(memberId = memberId, workbookId = useCaseIn.workbookId)
-        )
     }
 }

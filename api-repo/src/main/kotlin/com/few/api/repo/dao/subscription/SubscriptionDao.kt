248c2b70c3d6249e5bd5d10d6c0880e29311a30a
belljun3395 <195850@jnu.ac.kr>
2024-07-01 13:13:59 +0900

    [Refactor/#120] 구독해지 후 구독하는 경우 고려하여 구독 API 수정 (#128)
    * feat: 재구독 고려시 필요 쿼리 추가 및 수정
    
    - reSubscribeWorkbookSubscription: 재구독 업데이트 쿼리
    - selectAllWorkbookSubscriptionStatus: 워크북에 대한 멤버의 구독 정보 조회 쿼리
    - countWorkbookMappedArticles: 워크북에 속한 아티클 조회 쿼리
    
    * refactor: 재구독 상황 고려 리펙토링

--- api-repo/src/main/kotlin/com/few/api/repo/dao/subscription/SubscriptionDao.kt
+++ api-repo/src/main/kotlin/com/few/api/repo/dao/subscription/SubscriptionDao.kt
@@ -3,7 +3,10 @@ package com.few.api.repo.dao.subscription
 import com.few.api.repo.dao.subscription.command.InsertWorkbookSubscriptionCommand
 import com.few.api.repo.dao.subscription.command.UpdateDeletedAtInAllSubscriptionCommand
 import com.few.api.repo.dao.subscription.command.UpdateDeletedAtInWorkbookSubscriptionCommand
-import com.few.api.repo.dao.subscription.query.CountWorkbookSubscriptionQuery
+import com.few.api.repo.dao.subscription.query.SelectAllWorkbookSubscriptionStatusQueryNotConsiderDeletedAt
+import com.few.api.repo.dao.subscription.record.WorkbookSubscriptionStatus
+import com.few.api.repo.dao.subscription.query.CountWorkbookMappedArticlesQuery
+import jooq.jooq_dsl.Tables.MAPPING_WORKBOOK_ARTICLE
 import jooq.jooq_dsl.Tables.SUBSCRIPTION
 import org.jooq.DSLContext
 import org.springframework.stereotype.Component
@@ -22,6 +25,14 @@ class SubscriptionDao(
             .fetchOne()
     }
 
+    fun reSubscribeWorkbookSubscription(command: InsertWorkbookSubscriptionCommand) {
+        dslContext.update(SUBSCRIPTION)
+            .set(SUBSCRIPTION.DELETED_AT, null as LocalDateTime?)
+            .where(SUBSCRIPTION.MEMBER_ID.eq(command.memberId))
+            .and(SUBSCRIPTION.TARGET_WORKBOOK_ID.eq(command.workbookId))
+            .execute()
+    }
+
     fun updateDeletedAtInWorkbookSubscription(command: UpdateDeletedAtInWorkbookSubscriptionCommand) {
         dslContext.update(SUBSCRIPTION)
             .set(SUBSCRIPTION.DELETED_AT, LocalDateTime.now())
@@ -31,13 +42,16 @@ class SubscriptionDao(
             .execute()
     }
 
-    fun selectCountWorkbookSubscription(query: CountWorkbookSubscriptionQuery): Int {
-        return dslContext.selectCount()
+    fun selectAllWorkbookSubscriptionStatus(query: SelectAllWorkbookSubscriptionStatusQueryNotConsiderDeletedAt): List<WorkbookSubscriptionStatus> {
+        return dslContext.select(
+            SUBSCRIPTION.ID.`as`(WorkbookSubscriptionStatus::id.name),
+            SUBSCRIPTION.DELETED_AT.isNotNull.`as`(WorkbookSubscriptionStatus::subHistory.name),
+            SUBSCRIPTION.PROGRESS.add(1).`as`(WorkbookSubscriptionStatus::day.name)
+        )
             .from(SUBSCRIPTION)
             .where(SUBSCRIPTION.MEMBER_ID.eq(query.memberId))
             .and(SUBSCRIPTION.TARGET_WORKBOOK_ID.eq(query.workbookId))
-            .and(SUBSCRIPTION.DELETED_AT.isNull)
-            .fetchOne(0, Int::class.java) ?: 0
+            .fetchInto(WorkbookSubscriptionStatus::class.java)
     }
 
     fun updateDeletedAtInAllSubscription(command: UpdateDeletedAtInAllSubscriptionCommand) {
@@ -46,5 +60,12 @@ class SubscriptionDao(
             .set(SUBSCRIPTION.UNSUBS_OPINION, command.opinion) // TODO: opinion row 마다 중복 해결
             .where(SUBSCRIPTION.MEMBER_ID.eq(command.memberId))
             .execute()
+    }
+
+    fun countWorkbookMappedArticles(query: CountWorkbookMappedArticlesQuery): Int? {
+        return dslContext.selectCount()
+            .from(MAPPING_WORKBOOK_ARTICLE)
+            .where(MAPPING_WORKBOOK_ARTICLE.WORKBOOK_ID.eq(query.workbookId))
+            .fetchOne(0, Int::class.java)
     }
 }

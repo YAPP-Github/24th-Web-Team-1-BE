0cff6fe3bd780858fe739fa96b98a41a5c2361dc
belljun3395 <195850@jnu.ac.kr>
2024-07-01 09:52:41 +0900

    feat: MD 파일 Html 변환을 위한 의존성 추가

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -11,4 +11,10 @@ dependencies {
     implementation("io.minio:minio:${DependencyVersion.MINIO}")
     // s3
     implementation("com.amazonaws:aws-java-sdk-s3:${DependencyVersion.AWS_SDK}")
+
+    // commonmark - markdown to html
+    implementation("org.commonmark:commonmark:${DependencyVersion.COMMONMARK}")
+
+    // jsoup
+    implementation("org.jsoup:jsoup:1.15.3")
 }

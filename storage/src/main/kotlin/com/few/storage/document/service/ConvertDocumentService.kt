9940907520e9811245cc0e9a8cb2a3c9cf925f34
belljun3395 <195850@jnu.ac.kr>
2024-07-03 19:47:28 +0900

    refactor: html article 테그 추가 반영

--- storage/src/main/kotlin/com/few/storage/document/service/ConvertDocumentService.kt
+++ storage/src/main/kotlin/com/few/storage/document/service/ConvertDocumentService.kt
@@ -2,6 +2,7 @@ package com.few.storage.document.service
 
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
+import org.jsoup.Jsoup
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,10 +11,13 @@ class ConvertDocumentService {
     companion object {
         val parser = Parser.builder().build()
         val htmlRenderer = HtmlRenderer.builder().build()
+        val ARTICLE = "<article class='flex flex-col gap-1 body2-regular'> </article>"
     }
 
     fun mdToHtml(md: String): String {
+        val html = Jsoup.parse(ARTICLE)
         val article = htmlRenderer.render(parser.parse(md))
-        return article.toString()
+        html.body().append(article)
+        return html.toString()
     }
 }

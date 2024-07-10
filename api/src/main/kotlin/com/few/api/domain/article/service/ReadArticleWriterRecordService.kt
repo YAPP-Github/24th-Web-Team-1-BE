c924dd41c94e1303664fa08285855c5807be53ce
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-10 20:49:10 +0900

    refactor: article 도메인 서비스 out dto 적용

--- api/src/main/kotlin/com/few/api/domain/article/service/ReadArticleWriterRecordService.kt
+++ api/src/main/kotlin/com/few/api/domain/article/service/ReadArticleWriterRecordService.kt
@@ -1,9 +1,9 @@
 package com.few.api.domain.article.service
 
+import com.few.api.domain.article.service.dto.ReadWriterOutDto
 import com.few.api.domain.article.service.dto.ReadWriterRecordInDto
 import com.few.api.repo.dao.member.MemberDao
 import com.few.api.repo.dao.member.query.SelectWriterQuery
-import com.few.api.repo.dao.member.record.WriterRecord
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,9 +13,16 @@ class ReadArticleWriterRecordService(
 ) {
 
     @Transactional(readOnly = true)
-    fun execute(query: ReadWriterRecordInDto): WriterRecord? {
+    fun execute(query: ReadWriterRecordInDto): ReadWriterOutDto? {
         SelectWriterQuery(query.writerId).let { query: SelectWriterQuery ->
-            return memberDao.selectWriter(query)
+            val record = memberDao.selectWriter(query)
+            return record?.let {
+                ReadWriterOutDto(
+                    writerId = it.writerId,
+                    name = it.name,
+                    url = it.url
+                )
+            }
         }
     }
 }
---
+++ api/src/main/kotlin/com/few/api/domain/article/service/dto/BrowseArticleProblemsOutDto.kt
@@ -0,0 +1,5 @@
+package com.few.api.domain.article.service.dto
+
+data class BrowseArticleProblemsOutDto(
+    val problemIds: List<Long>
+)

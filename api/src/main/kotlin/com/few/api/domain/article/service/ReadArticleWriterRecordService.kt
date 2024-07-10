7024f6b1bd68c2d79135a77623af1c86564734ca
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-10 20:50:19 +0900

    refactor: article service에서 transactional 삭제

--- api/src/main/kotlin/com/few/api/domain/article/service/ReadArticleWriterRecordService.kt
+++ api/src/main/kotlin/com/few/api/domain/article/service/ReadArticleWriterRecordService.kt
@@ -5,14 +5,12 @@ import com.few.api.domain.article.service.dto.ReadWriterRecordInDto
 import com.few.api.repo.dao.member.MemberDao
 import com.few.api.repo.dao.member.query.SelectWriterQuery
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ReadArticleWriterRecordService(
     private val memberDao: MemberDao
 ) {
 
-    @Transactional(readOnly = true)
     fun execute(query: ReadWriterRecordInDto): ReadWriterOutDto? {
         SelectWriterQuery(query.writerId).let { query: SelectWriterQuery ->
             val record = memberDao.selectWriter(query)

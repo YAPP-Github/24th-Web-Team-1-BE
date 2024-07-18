21c0ad4f4d7d194a5528d2d61ad7c3b810e7c039
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-19 01:03:29 +0900

    fix: add ArticleViewHisAsyncEvent

---
+++ api/src/main/kotlin/com/few/api/domain/article/event/ArticleViewHisAsyncEvent.kt
@@ -0,0 +1,27 @@
+package com.few.api.domain.article.event
+
+import com.few.api.config.DatabaseAccessThreadPoolConfig.Companion.DATABASE_ACCESS_POOL
+import com.few.api.repo.dao.article.ArticleViewHisDao
+import com.few.api.repo.dao.article.command.ArticleViewHisCommand
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+class ArticleViewHisAsyncEvent(
+    private val articleViewHisDao: ArticleViewHisDao,
+) {
+    private val log = KotlinLogging.logger {}
+
+    @Async(value = DATABASE_ACCESS_POOL)
+    @Transactional
+    fun addArticleViewHis(articleId: Long, memberId: Long) {
+        try {
+            articleViewHisDao.insertArticleViewHis(ArticleViewHisCommand(articleId, memberId))
+            log.debug { "Successfully inserted article view history for articleId: $articleId and memberId: $memberId" }
+        } catch (e: Exception) {
+            log.error { "Failed to insert article view history for articleId: $articleId and memberId: $memberId" }
+        }
+    }
+}
--- api/src/main/kotlin/com/few/api/domain/article/usecase/ReadArticleUseCase.kt
+++ api/src/main/kotlin/com/few/api/domain/article/usecase/ReadArticleUseCase.kt
@@ -1,6 +1,6 @@
 package com.few.api.domain.article.usecase
 
-import com.few.api.config.DatabaseAccessThreadPoolConfig.Companion.DATABASE_ACCESS_POOL
+import com.few.api.domain.article.event.ArticleViewHisAsyncEvent
 import com.few.api.domain.article.usecase.dto.ReadArticleUseCaseIn
 import com.few.api.domain.article.usecase.dto.ReadArticleUseCaseOut
 import com.few.api.domain.article.usecase.dto.WriterDetail
@@ -11,12 +11,9 @@ import com.few.api.domain.article.service.dto.ReadWriterRecordInDto
 import com.few.api.exception.common.NotFoundException
 import com.few.api.repo.dao.article.ArticleDao
 import com.few.api.repo.dao.article.ArticleViewHisDao
-import com.few.api.repo.dao.article.command.ArticleViewHisCommand
 import com.few.api.repo.dao.article.query.ArticleViewHisCountQuery
 import com.few.api.repo.dao.article.query.SelectArticleRecordQuery
 import com.few.data.common.code.CategoryType
-import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -26,9 +23,8 @@ class ReadArticleUseCase(
     private val readArticleWriterRecordService: ReadArticleWriterRecordService,
     private val browseArticleProblemsService: BrowseArticleProblemsService,
     private val articleViewHisDao: ArticleViewHisDao,
+    private val articleViewHisAsyncEvent: ArticleViewHisAsyncEvent,
 ) {
-
-    private val log = KotlinLogging.logger {}
 
     @Transactional(readOnly = true)
     fun execute(useCaseIn: ReadArticleUseCaseIn): ReadArticleUseCaseOut {
@@ -47,7 +43,7 @@ class ReadArticleUseCase(
 
         val views = (articleViewHisDao.countArticleViews(ArticleViewHisCountQuery(useCaseIn.articleId)) ?: 0L) + 1L
 
-        insertArticleViewHisAsync(useCaseIn.articleId, useCaseIn.memberId)
+        articleViewHisAsyncEvent.addArticleViewHis(useCaseIn.articleId, useCaseIn.memberId)
 
         return ReadArticleUseCaseOut(
             id = articleRecord.articleId,
@@ -63,16 +59,5 @@ class ReadArticleUseCase(
             createdAt = articleRecord.createdAt,
             views = views
         )
-    }
-
-    @Async(value = DATABASE_ACCESS_POOL)
-    @Transactional
-    fun insertArticleViewHisAsync(articleId: Long, memberId: Long) {
-        try {
-            articleViewHisDao.insertArticleViewHis(ArticleViewHisCommand(articleId, memberId))
-            log.debug { "Successfully inserted article view history for articleId: $articleId and memberId: $memberId" }
-        } catch (e: Exception) {
-            log.error { "Failed to insert article view history for articleId: $articleId and memberId: $memberId" }
-        }
     }
 }

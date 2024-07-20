1c16093026f2e57a8d75776e0524dbe198622a86
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-20 15:08:25 +0900

    feat: ARTICLE_VIEW_COUNT 테이블 생성, 아티클 조회수 순 정렬 등에 사용 예정

---
+++ data/db/migration/entity/V1.00.0.14__manage_article_view_count.sql
@@ -0,0 +1,11 @@
+-- article 별 조회수 저장 테이블
+CREATE TABLE ARTICLE_VIEW_COUNT
+(
+    article_id BIGINT NOT NULL,
+    view_count BIGINT NOT NULL,
+    CONSTRAINT article_view_count_pk PRIMARY KEY (article_id)
+);
+
+-- 조회수 순으로 아티클 조회시 사용하기 위한 인덱스
+-- ex. SELECT * FROM ARTICLE_VIEW_COUNT ORDER BY view_count;
+CREATE INDEX article_view_count_idx ON ARTICLE_VIEW_COUNT (view_count);

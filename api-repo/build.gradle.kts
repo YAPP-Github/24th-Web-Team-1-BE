ad145e334d2778834deb3e127bf42c1e0d5c1ab6
belljun3395 <195850@jnu.ac.kr>
2024-07-23 12:20:19 +0900

    refactor: jooq starter 의존성 사용 가능으로 중복 선언 의존성 삭제

--- api-repo/build.gradle.kts
+++ api-repo/build.gradle.kts
@@ -10,15 +10,11 @@ dependencies {
     /** module */
     api(project(":data"))
 
-    /** spring starter */
-    api("org.springframework.boot:spring-boot-starter-data-jdbc")
+    /** mysql */
     implementation("com.mysql:mysql-connector-j")
 
     /** jooq */
-    implementation("org.springframework.boot:spring-boot-starter-jooq")
-    implementation("org.jooq:jooq:${DependencyVersion.JOOQ}")
-    implementation("org.jooq:jooq-meta:${DependencyVersion.JOOQ}")
-    implementation("org.jooq:jooq-codegen:${DependencyVersion.JOOQ}")
+    api("org.springframework.boot:spring-boot-starter-jooq")
     jooqCodegen("org.jooq:jooq-meta-extensions:${DependencyVersion.JOOQ}")
 
     /** flyway */

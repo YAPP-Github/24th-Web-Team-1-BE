c5282b7a6ad29a97ec9063f04de50fe1376d9e5e
belljun3395 <195850@jnu.ac.kr>
2024-07-23 12:21:35 +0900

    feat: PerformanceListener 생성

---
+++ api-repo/src/main/kotlin/com/few/api/repo/common/PerformanceListener.kt
@@ -0,0 +1,23 @@
+package com.few.api.repo.common
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.jooq.ExecuteContext
+import org.jooq.ExecuteListener
+import org.jooq.tools.StopWatch
+
+class PerformanceListener : ExecuteListener {
+    private val log = KotlinLogging.logger {}
+
+    private var watch: StopWatch? = null
+    override fun executeStart(ctx: ExecuteContext) {
+        super.executeStart(ctx)
+        watch = StopWatch()
+    }
+
+    override fun executeEnd(ctx: ExecuteContext) {
+        super.executeEnd(ctx)
+        if (watch!!.split() > 5000000000L) {
+            log.warn { "Slow Query Detected: ${ctx.query()}" }
+        }
+    }
+}

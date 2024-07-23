f1deb2ac7e19663bcf0173d0c4152e1efc0a2c05
belljun3395 <195850@jnu.ac.kr>
2024-07-23 12:21:48 +0900

    feat: JooqConfig 생성

---
+++ api-repo/src/main/kotlin/com/few/api/repo/config/JooqConfig.kt
@@ -0,0 +1,43 @@
+package com.few.api.repo.config
+
+import com.few.api.repo.common.ExceptionTranslator
+import com.few.api.repo.common.NativeSQLLogger
+import com.few.api.repo.common.PerformanceListener
+import org.jooq.SQLDialect
+import org.jooq.impl.DataSourceConnectionProvider
+import org.jooq.impl.DefaultConfiguration
+import org.jooq.impl.DefaultDSLContext
+import org.jooq.impl.DefaultExecuteListenerProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class JooqConfig(
+    private val dataSource: DataSource,
+) {
+    @Bean
+    fun dsl(): DefaultDSLContext {
+        return DefaultDSLContext(configuration())
+    }
+
+    @Bean
+    fun configuration(): DefaultConfiguration {
+        val jooqConfiguration = DefaultConfiguration()
+        jooqConfiguration.set(connectionProvider())
+        jooqConfiguration.set(DefaultExecuteListenerProvider(exceptionTransformer()))
+        jooqConfiguration.set(NativeSQLLogger(), PerformanceListener())
+        jooqConfiguration.set(SQLDialect.MYSQL)
+        return jooqConfiguration
+    }
+
+    @Bean
+    fun connectionProvider(): DataSourceConnectionProvider {
+        return DataSourceConnectionProvider(dataSource)
+    }
+
+    @Bean
+    fun exceptionTransformer(): ExceptionTranslator {
+        return ExceptionTranslator()
+    }
+}

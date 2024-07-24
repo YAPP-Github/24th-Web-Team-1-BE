ad88504ce863e2bd3e227ccb58befd81333982a0
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-25 00:08:05 +0900

    test: 아티클 목록 조회 API reponse body description 추가

--- api/src/test/kotlin/com/few/api/web/controller/article/ArticleControllerTest.kt
+++ api/src/test/kotlin/com/few/api/web/controller/article/ArticleControllerTest.kt
@@ -127,7 +127,11 @@ class ArticleControllerTest : ControllerTestSpec() {
                                         PayloadDocumentation.fieldWithPath("data.views")
                                             .fieldWithNumber("아티클 조회수"),
                                         PayloadDocumentation.fieldWithPath("data.includedWorkbooks")
-                                            .fieldWithArray("아티클이 포함된 학습지 정보(해당 API에서 사용되지 않음)")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(해당 API에서 사용되지 않음)"),
+                                        PayloadDocumentation.fieldWithPath("data.includedWorkbooks.id")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(학습지ID)(해당 API에서 사용되지 않음)"),
+                                        PayloadDocumentation.fieldWithPath("data.includedWorkbooks.title")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(학습지 제목)(해당 API에서 사용되지 않음)")
                                     )
                                 )
                             ).build()
@@ -213,7 +217,11 @@ class ArticleControllerTest : ControllerTestSpec() {
                                         PayloadDocumentation.fieldWithPath("data.articles.views")
                                             .fieldWithNumber("아티클 조회수"),
                                         PayloadDocumentation.fieldWithPath("data.articles.includedWorkbooks")
-                                            .fieldWithArray("아티클이 포함된 학습지 정보")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보"),
+                                        PayloadDocumentation.fieldWithPath("data.articles.includedWorkbooks.id")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(학습지ID)"),
+                                        PayloadDocumentation.fieldWithPath("data.articles.includedWorkbooks.title")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(학습지 제목)")
                                     )
                                 )
                             ).build()

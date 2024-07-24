7f3ce10bb62789f82dcd50b4c03ee925788cc5ba
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-24 23:52:53 +0900

    test: 응답 바디 type descrition 수정 (fieldWithObject -> fieldWithArray)

--- api/src/test/kotlin/com/few/api/web/controller/article/ArticleControllerTest.kt
+++ api/src/test/kotlin/com/few/api/web/controller/article/ArticleControllerTest.kt
@@ -127,7 +127,7 @@ class ArticleControllerTest : ControllerTestSpec() {
                                         PayloadDocumentation.fieldWithPath("data.views")
                                             .fieldWithNumber("아티클 조회수"),
                                         PayloadDocumentation.fieldWithPath("data.includedWorkbooks")
-                                            .fieldWithObject("아티클이 포함된 학습지 정보(해당 API에서 사용되지 않음)")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보(해당 API에서 사용되지 않음)")
                                     )
                                 )
                             ).build()
@@ -213,7 +213,7 @@ class ArticleControllerTest : ControllerTestSpec() {
                                         PayloadDocumentation.fieldWithPath("data.articles.views")
                                             .fieldWithNumber("아티클 조회수"),
                                         PayloadDocumentation.fieldWithPath("data.articles.includedWorkbooks")
-                                            .fieldWithObject("아티클이 포함된 학습지 정보")
+                                            .fieldWithArray("아티클이 포함된 학습지 정보")
                                     )
                                 )
                             ).build()

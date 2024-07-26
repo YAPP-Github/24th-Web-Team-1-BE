6bc7005f8b2912802682a2ba561dbb3c7a41a143
belljun3395 <195850@jnu.ac.kr>
2024-07-26 12:10:32 +0900

    chore: 코드 줄바꿈 린트 적용

--- api-repo/src/main/kotlin/com/few/api/repo/common/NativeSQLLogger.kt
+++ api-repo/src/main/kotlin/com/few/api/repo/common/NativeSQLLogger.kt
@@ -7,6 +7,7 @@ import org.jooq.impl.DSL
 
 class NativeSQLLogger : ExecuteListener {
     private val log = KotlinLogging.logger {}
+
     override fun executeEnd(ctx: ExecuteContext) {
         ctx.query()?.let {
             log.debug { "Query executed: ${DSL.using(ctx.configuration()).renderInlined(ctx.query())}" }

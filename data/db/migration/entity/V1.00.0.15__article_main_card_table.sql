cadc248f6e36860331d6353703f8ca267c53bc37
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-27 20:16:55 +0900

    docs: flyway V1.00.0.15 배포

---
+++ data/db/migration/entity/V1.00.0.15__article_main_card_table.sql
@@ -0,0 +1,16 @@
+-- article main card 테이블 (main card 뷰 용 조합 테이블)
+CREATE TABLE ARTICLE_MAIN_CARD
+(
+    id                 BIGINT       NOT NULL,
+    title              varchar(255) NOT NULL,
+    main_image_url     varchar(255) NOT NULL,
+    category_cd        TINYINT      NOT NULL,
+    created_at         timestamp    NOT NULL,
+    content            TEXT         NOT NULL,
+    member_id          BIGINT       NOT NULL,
+    email              varchar(255) NOT NULL,
+    member_type_cd     tinyint      NOT NULL,
+    member_description JSON         NOT NULL,
+    workbook_titles    JSON         NOT NULL,
+    CONSTRAINT article_main_card_pk PRIMARY KEY (article_id)
+);

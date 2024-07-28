d9027cdb0e7d8e0de713fffcc0f8cf04dfdd1541
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-28 15:37:07 +0900

    fix: ARTICLE_MAIN_CARD 테이블 컬럼 타입 변경

--- data/db/migration/entity/V1.00.0.15__article_main_card_table.sql
+++ data/db/migration/entity/V1.00.0.15__article_main_card_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE ARTICLE_MAIN_CARD
     created_at     timestamp     NOT NULL,
     member_id      BIGINT        NOT NULL,
     member_email   varchar(255)  NOT NULL,
-    member_name    JSON          NOT NULL,
-    member_img_url JSON          NOT NULL,
+    member_name    varchar(255)  NOT NULL,
+    member_img_url varchar(1000) NOT NULL,
     workbook_list  JSON          NOT NULL,
     CONSTRAINT article_main_card_pk PRIMARY KEY (id)
 );

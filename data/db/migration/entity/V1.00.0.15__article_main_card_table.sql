251021985200ca6fb27f1626f5cdd2a33fdecaa8
Jihun-Hwang <hun.ca.dev@gmail.com>
2024-07-28 13:46:24 +0900

    fix: ARTICLE_MAIN_CARD 컬럼 수정

--- data/db/migration/entity/V1.00.0.15__article_main_card_table.sql
+++ data/db/migration/entity/V1.00.0.15__article_main_card_table.sql
@@ -1,15 +1,15 @@
 -- article main card 테이블 (main card 뷰 용 조합 테이블)
 CREATE TABLE ARTICLE_MAIN_CARD
 (
-    id                 BIGINT       NOT NULL,
-    title              varchar(255) NOT NULL,
-    main_image_url     varchar(255) NOT NULL,
-    category_cd        TINYINT      NOT NULL,
-    created_at         timestamp    NOT NULL,
-    member_id          BIGINT       NOT NULL,
-    email              varchar(255) NOT NULL,
-    member_type_cd     tinyint      NOT NULL,
-    member_description JSON         NOT NULL,
-    workbook_titles    JSON         NOT NULL,
+    id             BIGINT        NOT NULL,
+    title          varchar(255)  NOT NULL,
+    main_image_url varchar(1000) NOT NULL,
+    category_cd    TINYINT       NOT NULL,
+    created_at     timestamp     NOT NULL,
+    member_id      BIGINT        NOT NULL,
+    member_email   varchar(255)  NOT NULL,
+    member_name    JSON          NOT NULL,
+    member_img_url JSON          NOT NULL,
+    workbook_list  JSON          NOT NULL,
     CONSTRAINT article_main_card_pk PRIMARY KEY (id)
 );

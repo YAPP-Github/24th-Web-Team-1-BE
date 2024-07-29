216baa953c538f87b0e31acc1dd69362538d2472
belljun3395 <195850@jnu.ac.kr>
2024-07-29 22:25:28 +0900

    refactor: AuthToken 멤버 접근 범위 수정

--- api/src/main/kotlin/com/few/api/security/token/AuthToken.kt
+++ api/src/main/kotlin/com/few/api/security/token/AuthToken.kt
@@ -1,6 +1,6 @@
 package com.few.api.security.token
 
 data class AuthToken(
-    private val accessToken: String,
-    private val refreshToken: String,
+    val accessToken: String,
+    val refreshToken: String,
 )

3d1c8fa65e23e62655b2d33e48d15c4385e82fb5
belljun3395 <195850@jnu.ac.kr>
2024-08-01 14:20:55 +0900

    feat: acm 적용

--- infra/terraform/aws/acm.tf
+++ infra/terraform/aws/acm.tf
@@ -1,12 +1,12 @@
-#resource "aws_acm_certificate" "cert" {
-#  domain_name       = "*.${var.domain_name}"
-#  validation_method = "DNS"
-#
-#  tags = {
-#    Environment = "${var.prefix}"
-#  }
-#
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "${var.prefix}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- infra/terraform/variable.tf
+++ infra/terraform/variable.tf
@@ -7,7 +7,7 @@ variable "prefix" {
 variable "domain_name" {
   type        = string
   description = "The domain name for the Route 53 hosted zone"
-  default     = "https://api.fewletter.site"
+  default     = "api.fewletter.site"
 }
 
 variable "ncp_access_key" {

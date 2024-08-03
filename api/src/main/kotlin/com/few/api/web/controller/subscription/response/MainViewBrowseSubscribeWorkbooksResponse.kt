e90dbac98d5c4a1eea0dcc754c2a8364fc9b8792
belljun3395 <195850@jnu.ac.kr>
2024-08-03 21:45:30 +0900

    feat: mainViewBrowseSubscribeWorkbooks 구현

---
+++ api/src/main/kotlin/com/few/api/web/controller/subscription/response/MainViewBrowseSubscribeWorkbooksResponse.kt
@@ -0,0 +1,30 @@
+package com.few.api.web.controller.subscription.response
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.few.api.domain.workbook.usecase.dto.WriterDetail
+import java.net.URL
+import java.time.LocalDateTime
+
+data class MainViewBrowseSubscribeWorkbooksResponse(
+    val workbooks: List<MainViewSubscribeWorkbookInfo>,
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class MainViewSubscribeWorkbookInfo(
+    val id: Long,
+    val mainImageUrl: URL,
+    val title: String,
+    val description: String,
+    val category: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    val createdAt: LocalDateTime,
+    val writerDetails: List<WriterDetail>,
+    val subscriptionCount: Long,
+    val status: String?, // convert from enum
+    val totalDay: Int?,
+    val currentDay: Int?,
+    val rank: Long?,
+    val totalSubscriber: Long?,
+    val articleInfo: String?, // convert from Json
+)

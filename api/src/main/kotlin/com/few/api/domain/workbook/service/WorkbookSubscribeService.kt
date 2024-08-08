b0374c07ee30f7e528b4c8780d7682e8718ffeeb
belljun3395 <195850@jnu.ac.kr>
2024-08-08 11:30:12 +0900

    refactor: 구독정보 조회 쿼리 분리 반영

--- api/src/main/kotlin/com/few/api/domain/workbook/service/WorkbookSubscribeService.kt
+++ api/src/main/kotlin/com/few/api/domain/workbook/service/WorkbookSubscribeService.kt
@@ -3,7 +3,8 @@ package com.few.api.domain.workbook.service
 import com.few.api.domain.workbook.service.dto.BrowseMemberSubscribeWorkbooksInDto
 import com.few.api.domain.workbook.service.dto.BrowseMemberSubscribeWorkbooksOutDto
 import com.few.api.repo.dao.subscription.SubscriptionDao
-import com.few.api.repo.dao.subscription.query.SelectAllMemberWorkbookSubscriptionStatusUnsubOpinionConditionAndNotConsiderDeletedAQuery
+import com.few.api.repo.dao.subscription.query.SelectAllMemberWorkbookActiveSubscription
+import com.few.api.repo.dao.subscription.query.SelectAllMemberWorkbookInActiveSubscription
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,14 +13,24 @@ class WorkbookSubscribeService(
 ) {
 
     fun browseMemberSubscribeWorkbooks(dto: BrowseMemberSubscribeWorkbooksInDto): List<BrowseMemberSubscribeWorkbooksOutDto> {
-        return SelectAllMemberWorkbookSubscriptionStatusUnsubOpinionConditionAndNotConsiderDeletedAQuery(dto.memberId).let { it ->
-            subscriptionDao.selectAllWorkbookSubscriptionStatus(it).map {
-                BrowseMemberSubscribeWorkbooksOutDto(
-                    workbookId = it.workbookId,
-                    isActiveSub = it.isActiveSub,
-                    currentDay = it.currentDay
-                )
+        val inActiveSubscriptionRecords =
+            SelectAllMemberWorkbookInActiveSubscription(dto.memberId).let {
+                subscriptionDao.selectAllInActiveWorkbookSubscriptionStatus(it)
             }
+
+        val activeSubscriptionRecords =
+            SelectAllMemberWorkbookActiveSubscription(dto.memberId).let {
+                subscriptionDao.selectAllActiveWorkbookSubscriptionStatus(it)
+            }
+
+        val subscriptionRecords = inActiveSubscriptionRecords + activeSubscriptionRecords
+
+        return subscriptionRecords.map {
+            BrowseMemberSubscribeWorkbooksOutDto(
+                workbookId = it.workbookId,
+                isActiveSub = it.isActiveSub,
+                currentDay = it.currentDay
+            )
         }
     }
 }

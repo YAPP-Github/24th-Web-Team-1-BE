31c904df575ffb7c77881eee6b68235cc02fa29c
belljun3395 <195850@jnu.ac.kr>
2024-08-14 16:39:37 +0900

    refactor: EmailSender 구현 수정

--- email/src/main/kotlin/com/few/email/service/member/SendAuthEmailService.kt
+++ email/src/main/kotlin/com/few/email/service/member/SendAuthEmailService.kt
@@ -1,20 +1,19 @@
 package com.few.email.service.member
 
-import com.few.email.sender.SendEmailSender
+import com.few.email.sender.EmailSender
+import com.few.email.sender.provider.EmailSendProvider
 import com.few.email.service.member.dto.SendAuthEmailArgs
 import org.springframework.boot.autoconfigure.mail.MailProperties
-import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Component
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
-import java.util.*
 
 @Component
 class SendAuthEmailService(
     mailProperties: MailProperties,
-    emailSender: JavaMailSender,
+    emailSendProvider: EmailSendProvider,
     private val templateEngine: TemplateEngine,
-) : SendEmailSender<SendAuthEmailArgs>(mailProperties, emailSender) {
+) : EmailSender<SendAuthEmailArgs>(mailProperties, emailSendProvider) {
 
     override fun getHtml(args: SendAuthEmailArgs): String {
         val context = Context()

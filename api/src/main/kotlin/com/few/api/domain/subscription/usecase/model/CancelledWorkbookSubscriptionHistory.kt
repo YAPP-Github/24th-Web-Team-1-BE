dd4fb328b6bdf038cd6d1b46fe3ce223f61cceb8
belljun3395 <195850@jnu.ac.kr>
2024-08-28 03:11:22 +0900

    [Refactor/#363] SubscribeWorkbookUseCase 모델 변환 (#365)
    * feat: WorkbookSubscriptionStatus 구현
    
    * feat: WorkbookSubscriptionHistory 구현
    
    * feat: CancelledWorkbookSubscriptionHistory 구현
    
    * test: WorkbookSubscriptionHistoryTest 구현
    
    * refactor: Subscribe 모델 적용하여 리펙토링
    
    * refactor: workbookSubscriptionStatus 접근 제어자 protected로 수정
    
    * chore: init 메서드 위치 수정
    
    * refactor: 생성자 변경에 따른 테스트 수정

---
+++ api/src/main/kotlin/com/few/api/domain/subscription/usecase/model/CancelledWorkbookSubscriptionHistory.kt
@@ -0,0 +1,21 @@
+package com.few.api.domain.subscription.usecase.model
+
+class CancelledWorkbookSubscriptionHistory(
+    workbookSubscriptionHistory: WorkbookSubscriptionHistory,
+) : WorkbookSubscriptionHistory(
+    workbookSubscriptionHistory
+) {
+
+    init {
+        require(isCancelSub) {
+            "CanceledWorkbookSubscriptionHistory is not for active subscription."
+        }
+    }
+
+    /**
+     * 구독이 종료되었는지 여부 확인
+     */
+    fun isSubEnd(lastDay: Int): Boolean {
+        return (lastDay <= workbookSubscriptionStatus!!.day)
+    }
+}

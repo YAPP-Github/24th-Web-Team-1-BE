de4ae5a7b351fba1247672dcee65fd1719c9f4b3
belljun3395 <195850@jnu.ac.kr>
2024-09-05 15:52:54 +0900

    refactor: /subscriptions/workbooks view에 따른 구분 및 /subscriptions/workbooks/main 삭제

--- api/src/main/kotlin/com/few/api/web/controller/subscription/SubscriptionController.kt
+++ api/src/main/kotlin/com/few/api/web/controller/subscription/SubscriptionController.kt
@@ -7,23 +7,11 @@ import com.few.api.web.support.ApiResponseGenerator
 import com.few.api.domain.subscription.usecase.SubscribeWorkbookUseCase
 import com.few.api.domain.subscription.usecase.UnsubscribeAllUseCase
 import com.few.api.domain.subscription.usecase.UnsubscribeWorkbookUseCase
-import com.few.api.domain.subscription.usecase.dto.BrowseSubscribeWorkbooksUseCaseIn
-import com.few.api.domain.subscription.usecase.dto.SubscribeWorkbookUseCaseIn
-import com.few.api.domain.subscription.usecase.dto.UnsubscribeAllUseCaseIn
-import com.few.api.domain.subscription.usecase.dto.UnsubscribeWorkbookUseCaseIn
-import com.few.api.domain.workbook.usecase.BrowseWorkbooksUseCase
-import com.few.api.domain.workbook.usecase.dto.BrowseWorkbooksUseCaseIn
+import com.few.api.domain.subscription.usecase.dto.*
 import com.few.api.security.authentication.token.TokenUserDetails
-import com.few.api.security.filter.token.AccessTokenResolver
-import com.few.api.security.token.TokenResolver
 import com.few.api.web.controller.subscription.request.UnsubscribeAllRequest
-import com.few.api.web.controller.subscription.response.MainViewBrowseSubscribeWorkbooksResponse
-import com.few.api.web.controller.subscription.response.MainViewSubscribeWorkbookInfo
-import com.few.api.web.controller.subscription.response.SubscribeWorkbookInfo
-import com.few.api.web.controller.subscription.response.SubscribeWorkbooksResponse
+import com.few.api.web.controller.subscription.response.*
 import com.few.api.web.support.ViewCategory
-import com.few.api.web.support.WorkBookCategory
-import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Min
 import org.springframework.http.HttpStatus
@@ -31,6 +19,7 @@ import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import java.lang.IllegalStateException
 
 @Validated
 @RestController
@@ -40,10 +29,6 @@ class SubscriptionController(
     private val unsubscribeWorkbookUseCase: UnsubscribeWorkbookUseCase,
     private val unsubscribeAllUseCase: UnsubscribeAllUseCase,
     private val browseSubscribeWorkbooksUseCase: BrowseSubscribeWorkbooksUseCase,
-    private val tokenResolver: TokenResolver,
-
-    // 임시 구현용
-    private val browseWorkBooksUseCase: BrowseWorkbooksUseCase,
 ) {
 
     @GetMapping("/subscriptions/workbooks")
@@ -52,110 +37,45 @@ class SubscriptionController(
         @RequestParam(
             value = "view",
             required = false
-        ) view: ViewCategory? = ViewCategory.MAIN_CARD,
+        ) view: ViewCategory?,
     ): ApiResponse<ApiResponse.SuccessBody<SubscribeWorkbooksResponse>> {
         val memberId = userDetails.username.toLong()
-        val useCaseOut = BrowseSubscribeWorkbooksUseCaseIn(memberId).let {
-            browseSubscribeWorkbooksUseCase.execute(it)
-        }
+        val useCaseOut =
+            BrowseSubscribeWorkbooksUseCaseIn(memberId, view ?: ViewCategory.MAIN_CARD).let {
+                browseSubscribeWorkbooksUseCase.execute(it)
+            }
 
+        // todo fix to facade usecase
         return SubscribeWorkbooksResponse(
-            workbooks = useCaseOut.workbooks.map {
-                SubscribeWorkbookInfo(
-                    id = it.workbookId,
-                    currentDay = it.currentDay,
-                    totalDay = it.totalDay,
-                    status = it.isActiveSub.name,
-                    rank = it.rank,
-                    totalSubscriber = it.totalSubscriber,
-                    articleInfo = it.articleInfo
-                )
+            workbooks = when (useCaseOut.clazz) {
+                MainCardSubscribeWorkbookDetail::class.java -> useCaseOut.workbooks.map { it as MainCardSubscribeWorkbookDetail }.map {
+                    MainCardSubscribeWorkbookInfo(
+                        id = it.workbookId,
+                        status = it.isActiveSub.name,
+                        totalDay = it.totalDay,
+                        currentDay = it.currentDay,
+                        rank = it.rank,
+                        totalSubscriber = it.totalSubscriber,
+                        subscription = it.subscription,
+                        articleInfo = it.articleInfo
+                    )
+                }
+                MyPageSubscribeWorkbookDetail::class.java -> useCaseOut.workbooks.map { it as MyPageSubscribeWorkbookDetail }.map {
+                    MyPageSubscribeWorkbookInfo(
+                        id = it.workbookId,
+                        status = it.isActiveSub.name,
+                        totalDay = it.totalDay,
+                        currentDay = it.currentDay,
+                        rank = it.rank,
+                        totalSubscriber = it.totalSubscriber,
+                        subscription = it.subscription,
+                        workbookInfo = it.workbookInfo
+                    )
+                }
+                else -> throw IllegalStateException("Invalid class type")
             }
         ).let {
             ApiResponseGenerator.success(it, HttpStatus.OK)
-        }
-    }
-
-    @GetMapping("/subscriptions/workbooks/main")
-    fun mainViewBrowseSubscribeWorkbooks(
-        servletRequest: HttpServletRequest,
-        @RequestParam(value = "category", required = false)
-        category: WorkBookCategory?,
-    ): ApiResponse<ApiResponse.SuccessBody<MainViewBrowseSubscribeWorkbooksResponse>> {
-        val authorization: String? = servletRequest.getHeader("Authorization")
-        val memberId = authorization?.let {
-            AccessTokenResolver.resolve(it)
-        }.let {
-            tokenResolver.resolveId(it)
-        }
-
-        if (memberId != null) {
-            val memberSubscribeWorkbooks = BrowseSubscribeWorkbooksUseCaseIn(memberId).let {
-                browseSubscribeWorkbooksUseCase.execute(it)
-            }
-            val workbooks =
-                BrowseWorkbooksUseCaseIn(
-                    category ?: WorkBookCategory.All,
-                    ViewCategory.MAIN_CARD,
-                    memberId
-                ).let { useCaseIn ->
-                    browseWorkBooksUseCase.execute(useCaseIn)
-                }
-
-            return MainViewBrowseSubscribeWorkbooksResponse(
-                workbooks = workbooks.workbooks.map {
-                    MainViewSubscribeWorkbookInfo(
-                        id = it.id,
-                        mainImageUrl = it.mainImageUrl,
-                        title = it.title,
-                        description = it.description,
-                        category = it.category,
-                        createdAt = it.createdAt,
-                        writerDetails = it.writerDetails,
-                        subscriptionCount = it.subscriptionCount,
-                        status = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.isActiveSub?.name,
-                        totalDay = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.totalDay,
-                        currentDay = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.currentDay,
-                        rank = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.rank,
-                        totalSubscriber = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.totalSubscriber,
-                        articleInfo = memberSubscribeWorkbooks.workbooks.find { subscribe -> subscribe.workbookId == it.id }?.articleInfo
-                    )
-                }
-            ).let {
-                ApiResponseGenerator.success(it, HttpStatus.OK)
-            }
-        } else {
-            val workbooks =
-                BrowseWorkbooksUseCaseIn(
-                    category ?: WorkBookCategory.All,
-                    ViewCategory.MAIN_CARD,
-                    memberId
-                ).let { useCaseIn ->
-                    browseWorkBooksUseCase.execute(useCaseIn)
-                }
-
-            return MainViewBrowseSubscribeWorkbooksResponse(
-                workbooks = workbooks.workbooks.map {
-                    MainViewSubscribeWorkbookInfo(
-                        id = it.id,
-                        mainImageUrl = it.mainImageUrl,
-                        title = it.title,
-                        description = it.description,
-                        category = it.category,
-                        createdAt = it.createdAt,
-                        writerDetails = it.writerDetails,
-                        subscriptionCount = it.subscriptionCount,
-                        status = null,
-                        totalDay = null,
-                        currentDay = null,
-                        rank = null,
-                        totalSubscriber = null,
-                        articleInfo = null
-                    )
-                }
-            ).let {
-                ApiResponseGenerator.success(it, HttpStatus.OK)
-            }
         }
     }
 

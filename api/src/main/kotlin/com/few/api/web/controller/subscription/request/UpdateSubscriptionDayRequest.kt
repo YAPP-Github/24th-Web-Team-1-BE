21889715351ab2274995ed80b87ceac794e07bc1
belljun3395 <195850@jnu.ac.kr>
2024-09-11 22:40:29 +0900

    [Refactor/#393] 구독 시간 수정 바디 파라미터 이름 수정 (#395)
    * refactor: dayCode -> date로 바디 키 변경
    
    * fix: 구독 관련 컨트롤러 테스트 수행되지 않는 문제 해결

--- api/src/main/kotlin/com/few/api/web/controller/subscription/request/UpdateSubscriptionDayRequest.kt
+++ api/src/main/kotlin/com/few/api/web/controller/subscription/request/UpdateSubscriptionDayRequest.kt
@@ -1,6 +1,9 @@
 package com.few.api.web.controller.subscription.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UpdateSubscriptionDayRequest(
+    @JsonProperty("date")
     val dayCode: String,
     val workbookId: Long?,
 )

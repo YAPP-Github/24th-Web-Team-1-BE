31c3490f7c87ae07695bd29e5315ec8aac241ef3
belljun3395 <195850@jnu.ac.kr>
2024-09-11 20:02:58 +0900

    refactor: AuthorityUtils 적용

--- api/src/main/kotlin/com/few/api/security/authentication/token/TokenUserDetailsService.kt
+++ api/src/main/kotlin/com/few/api/security/authentication/token/TokenUserDetailsService.kt
@@ -1,12 +1,10 @@
 package com.few.api.security.authentication.token
 
-import com.few.api.security.authentication.authority.Roles
+import com.few.api.security.authentication.authority.AuthorityUtils
 import com.few.api.security.exception.AccessTokenInvalidException
 import com.few.api.security.token.TokenResolver
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.jsonwebtoken.Claims
-import org.apache.commons.lang3.StringUtils
-import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
@@ -44,24 +42,8 @@ class TokenUserDetailsService(
             String::class.java
         )
 
-        val authorities = toAuthorities(roles)
+        val authorities = AuthorityUtils.toAuthorities(roles)
 
         return TokenUserDetails(authorities, id.toString(), email)
-    }
-
-    private fun toAuthorities(roles: String): List<GrantedAuthority> {
-        val tokens = StringUtils.splitPreserveAllTokens(roles, "[,]")
-        val rtn: MutableList<GrantedAuthority> = ArrayList()
-        for (token in tokens) {
-            if (token != "") {
-                val role = token.trim { it <= ' ' }
-                try {
-                    rtn.add(Roles.valueOf(role).authority)
-                } catch (exception: IllegalArgumentException) {
-                    log.error { "${"Invalid role. role: {}"} $role" }
-                }
-            }
-        }
-        return rtn
     }
 }

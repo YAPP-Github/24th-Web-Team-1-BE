5aeecc7e5836533bbd575c9334e7ac1ec160533e
belljun3395 <195850@jnu.ac.kr>
2024-09-15 22:25:37 +0900

    refactor: WorkbookSubscriptionAfterCompletionEventListener에 트랜잭션 제거

--- api/src/main/kotlin/com/few/api/domain/subscription/event/WorkbookSubscriptionAfterCompletionEventListener.kt
+++ api/src/main/kotlin/com/few/api/domain/subscription/event/WorkbookSubscriptionAfterCompletionEventListener.kt
@@ -3,8 +3,6 @@ package com.few.api.domain.subscription.event
 import com.few.api.domain.subscription.event.dto.WorkbookSubscriptionEvent
 import com.few.api.domain.subscription.handler.SendWorkbookArticleAsyncHandler
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
@@ -14,7 +12,6 @@ class WorkbookSubscriptionAfterCompletionEventListener(
 ) {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun handleEvent(event: WorkbookSubscriptionEvent) {
         sendWorkbookArticleAsyncHandler.sendWorkbookArticle(
             event.memberId,

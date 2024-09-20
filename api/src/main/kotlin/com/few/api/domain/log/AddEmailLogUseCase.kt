507a026abb6f935946a55c2ae7277cc27d6ed914
belljun3395 <195850@jnu.ac.kr>
2024-09-21 00:22:20 +0900

    feat: AddEmailLogUseCase 구현

---
+++ api/src/main/kotlin/com/few/api/domain/log/AddEmailLogUseCase.kt
@@ -0,0 +1,53 @@
+package com.few.api.domain.log
+
+import com.few.api.domain.log.dto.AddEmailLogUseCaseIn
+import com.few.api.repo.dao.log.SendArticleEventHistoryDao
+import com.few.api.repo.dao.log.command.InsertEventCommand
+import com.few.api.repo.dao.log.query.SelectEventByMessageIdAndEventTypeQuery
+import com.few.api.repo.dao.member.MemberDao
+import com.few.api.repo.dao.member.query.SelectMemberByEmailQuery
+import com.few.api.web.support.EmailLogEventType
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import org.webjars.NotFoundException
+
+@Component
+class AddEmailLogUseCase(
+    private val memberDao: MemberDao,
+    private val sendArticleEventHistoryDao: SendArticleEventHistoryDao,
+) {
+    @Transactional
+    fun execute(useCaseIn: AddEmailLogUseCaseIn) {
+        val (memberId, _, _, _) = memberDao.selectMemberByEmail(
+            SelectMemberByEmailQuery(useCaseIn.destination[0])
+        ) ?: throw NotFoundException("member.notfound.email")
+
+        val record =
+            sendArticleEventHistoryDao.selectEventByMessageId(
+                SelectEventByMessageIdAndEventTypeQuery(
+                    useCaseIn.messageId,
+                    EmailLogEventType.SEND.code
+                )
+            )
+                ?: throw IllegalStateException("event is not found")
+
+        sendArticleEventHistoryDao.insertEvent(
+            InsertEventCommand(
+                memberId = memberId,
+                articleId = record.articleId,
+                messageId = record.messageId,
+                eventType = record.eventType,
+                sendType = record.sendType
+            )
+        ).let {
+            when (EmailLogEventType.fromCode(record.eventType)) {
+                EmailLogEventType.DELIVERYDELAY -> {
+                    TODO("배송지연 이벤트 발생시 처리 로직 추가")
+                }
+                else -> {
+                    TODO("다른 이벤트는 필요시 추가한다.")
+                }
+            }
+        }
+    }
+}

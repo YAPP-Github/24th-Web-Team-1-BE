605cb079cfffaf7bb2289976102bab00eca31706
belljun3395 <195850@jnu.ac.kr>
2024-10-10 14:53:32 +0900

    [Refactor/#427] BrowseWorkbooksUseCase 리펙토링 (#428)
    * feat: Workbooks 모델 생성
    
    * refactor: 기존 WorkbookOrderDelegator 이동 및 order 메서드 시그니쳐 변경
    
    * refactor: WorkbookOrderDelegator 구현체 이동 및 변경 반영
    
    * feat: OrderedWorkBooks 모델 생성
    
    * refactor: BrowseWorkbooksUseCase에 변경 반영
    
    * refactor: 변경 사항 테스트에 반영
    
    * refactor: WorkBook 클래스 범용 데이터 클래스로 변경
    
    * feat: order 모델에서 사용할 OrderTargetWorkBooks 구현
    
    * feat: order 모델 부모 객체 생성
    
    * refactor: order 모델 변경 사항 반영
    
    * refactor: 변경 사항 반영
    
    * refactor: orderedWorkbooks 공통으로 처리할 수 있도록 수정
    
    * fix: 변경 사항 테스트에 적용

--- api/src/main/kotlin/com/few/api/domain/workbook/usecase/BrowseWorkbooksUseCase.kt
+++ api/src/main/kotlin/com/few/api/domain/workbook/usecase/BrowseWorkbooksUseCase.kt
@@ -4,16 +4,16 @@ import com.few.api.domain.workbook.service.WorkbookMemberService
 import com.few.api.domain.workbook.service.WorkbookSubscribeService
 import com.few.api.domain.workbook.service.dto.BrowseMemberSubscribeWorkbooksInDto
 import com.few.api.domain.workbook.service.dto.BrowseWorkbookWriterRecordsInDto
+import com.few.api.domain.workbook.service.dto.WriterMappedWorkbookOutDto
 import com.few.api.domain.workbook.usecase.dto.BrowseWorkBookDetail
 import com.few.api.domain.workbook.usecase.dto.BrowseWorkbooksUseCaseIn
 import com.few.api.domain.workbook.usecase.dto.BrowseWorkbooksUseCaseOut
 import com.few.api.domain.workbook.usecase.dto.WriterDetail
 import com.few.api.domain.workbook.usecase.model.*
-import com.few.api.domain.workbook.usecase.service.order.AuthMainViewWorkbookOrderDelegator
-import com.few.api.domain.workbook.usecase.service.order.BasicWorkbookOrderDelegator
-import com.few.api.domain.workbook.usecase.service.order.WorkbookOrderDelegatorExecutor
+import com.few.api.domain.workbook.usecase.model.order.*
 import com.few.api.repo.dao.workbook.WorkbookDao
 import com.few.api.repo.dao.workbook.query.BrowseWorkBookQueryWithSubscriptionCountQuery
+import com.few.api.repo.dao.workbook.record.SelectWorkBookRecordWithSubscriptionCount
 import com.few.api.web.support.ViewCategory
 import com.few.data.common.code.CategoryType
 import org.springframework.stereotype.Component
@@ -42,7 +42,6 @@ class BrowseWorkbooksUseCase(
     private val workbookDao: WorkbookDao,
     private val workbookMemberService: WorkbookMemberService,
     private val workbookSubscribeService: WorkbookSubscribeService,
-    private val workbookOrderDelegatorExecutor: WorkbookOrderDelegatorExecutor,
 ) {
 
     @Transactional
@@ -56,54 +55,30 @@ class BrowseWorkbooksUseCase(
             BrowseWorkbookWriterRecordsInDto(workbookIds)
         )
 
-        val workbookDetails = workbookRecords.map { record ->
-            WorkBook(
-                id = record.id,
-                mainImageUrl = record.mainImageUrl,
-                title = record.title,
-                description = record.description,
-                category = CategoryType.convertToDisplayName(record.category),
-                createdAt = record.createdAt,
-                writerDetails = writerRecords[record.id]?.map {
-                    WorkBookWriter(
-                        id = it.writerId,
-                        name = it.name,
-                        url = it.url
-                    )
-                } ?: emptyList(),
-                subscriptionCount = record.subscriptionCount
-            )
-        }
-
-        val orderStrategy = when {
-            useCaseIn.viewCategory == ViewCategory.MAIN_CARD && useCaseIn.memberId != null -> WorkBookOrderStrategy.MAIN_VIEW_AUTH
-            useCaseIn.viewCategory == ViewCategory.MAIN_CARD && useCaseIn.memberId == null -> WorkBookOrderStrategy.MAIN_VIEW_UNAUTH
-            else -> WorkBookOrderStrategy.BASIC
-        }
-
-        val orderedWorkbooks = when (orderStrategy) {
+        val orderStrategy = getOrderStrategy(useCaseIn)
+        val orderDelegator = when (orderStrategy) {
             WorkBookOrderStrategy.MAIN_VIEW_AUTH -> {
-                BrowseMemberSubscribeWorkbooksInDto(useCaseIn.memberId!!).let { dto ->
-                    workbookSubscribeService.browseMemberSubscribeWorkbooks(dto)
-                }.map {
-                    MemberSubscribedWorkbook(
-                        workbookId = it.workbookId,
-                        isActiveSub = it.isActiveSub,
-                        currentDay = it.currentDay
-                    )
-                }.let { subscribedWorkbooks ->
-                    AuthMainViewWorkbookOrderDelegator(workbookDetails, subscribedWorkbooks)
-                }
+                genAuthMainViewWorkbookOrderDelegator(useCaseIn)
             }
-            WorkBookOrderStrategy.MAIN_VIEW_UNAUTH -> {
-                BasicWorkbookOrderDelegator(workbookDetails)
-            }
-            else -> BasicWorkbookOrderDelegator(workbookDetails)
-        }.let { delegator ->
-            workbookOrderDelegatorExecutor.execute(delegator)
+            /** BASIC, MAIN_VIEW_UNAUTH -> 해당 경우는 DB 조회 결과를 그대로 반환 */
+            else -> null
         }
 
-        orderedWorkbooks.map { workBook ->
+        val workbooks = toWorkbooks(workbookRecords, writerRecords)
+
+        val orderedWorkbook = OrderTargetWorkBooks(workbooks).let { target ->
+            orderDelegator
+                ?.let { delegator ->
+                    UnOrderedWorkBooks(
+                        target,
+                        delegator
+                    ).order()
+                }
+                ?: run { OrderedWorkBooks(target) }
+        }.orderedWorkbooks
+
+        val orderedWorkbookData = orderedWorkbook.workbookData
+        orderedWorkbookData.map { workBook ->
             BrowseWorkBookDetail(
                 id = workBook.id,
                 mainImageUrl = workBook.mainImageUrl,
@@ -124,6 +99,53 @@ class BrowseWorkbooksUseCase(
             return BrowseWorkbooksUseCaseOut(
                 workbooks = it
             )
+        }
+    }
+
+    private fun getOrderStrategy(useCaseIn: BrowseWorkbooksUseCaseIn) =
+        when {
+            useCaseIn.viewCategory == ViewCategory.MAIN_CARD && useCaseIn.memberId != null -> WorkBookOrderStrategy.MAIN_VIEW_AUTH
+            useCaseIn.viewCategory == ViewCategory.MAIN_CARD && useCaseIn.memberId == null -> WorkBookOrderStrategy.MAIN_VIEW_UNAUTH
+            else -> WorkBookOrderStrategy.BASIC
+        }
+
+    private fun genAuthMainViewWorkbookOrderDelegator(useCaseIn: BrowseWorkbooksUseCaseIn): WorkbookOrderDelegator {
+        return BrowseMemberSubscribeWorkbooksInDto(useCaseIn.memberId!!).let { dto ->
+            workbookSubscribeService.browseMemberSubscribeWorkbooks(dto)
+        }.map {
+            MemberSubscribedWorkbook(
+                workbookId = it.workbookId,
+                isActiveSub = it.isActiveSub,
+                currentDay = it.currentDay
+            )
+        }.let { subscribedWorkbooks ->
+            AuthMainViewWorkbookOrderDelegator(subscribedWorkbooks)
+        }
+    }
+
+    private fun toWorkbooks(
+        workbookRecords: List<SelectWorkBookRecordWithSubscriptionCount>,
+        writerRecords: Map<Long, List<WriterMappedWorkbookOutDto>>,
+    ): WorkBooks {
+        return workbookRecords.map { record ->
+            WorkBook(
+                id = record.id,
+                mainImageUrl = record.mainImageUrl,
+                title = record.title,
+                description = record.description,
+                category = CategoryType.convertToDisplayName(record.category),
+                createdAt = record.createdAt,
+                writerDetails = writerRecords[record.id]?.map {
+                    WorkBookWriter(
+                        id = it.writerId,
+                        name = it.name,
+                        url = it.url
+                    )
+                } ?: emptyList(),
+                subscriptionCount = record.subscriptionCount
+            )
+        }.let {
+            WorkBooks(it)
         }
     }
 }
---
+++ api/src/main/kotlin/com/few/api/domain/workbook/usecase/model/order/OrderTargetWorkBooks.kt
@@ -0,0 +1,7 @@
+package com.few.api.domain.workbook.usecase.model.order
+
+import com.few.api.domain.workbook.usecase.model.WorkBooks
+
+data class OrderTargetWorkBooks(
+    val workbooks: WorkBooks,
+)

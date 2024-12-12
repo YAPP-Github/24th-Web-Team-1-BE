231210808a333a2d797bdad78208d4ae7174c8be
belljun3395 <195850@jnu.ac.kr>
2024-12-12 12:22:39 +0900

    refactor: email 모듈에서 mail 관련 의존성 api에서 implementation으로 수정

--- email/build.gradle.kts
+++ email/build.gradle.kts
@@ -8,8 +8,8 @@ tasks.getByName("jar") {
 
 dependencies {
     /** starter */
-    api("org.springframework.boot:spring-boot-starter-mail")
-    api("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
     /** aws ses */
     implementation("com.amazonaws:aws-java-sdk-ses:${DependencyVersion.AWS_SES}")

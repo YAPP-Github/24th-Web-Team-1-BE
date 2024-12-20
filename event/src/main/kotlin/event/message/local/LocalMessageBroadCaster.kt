ebf3d5434d4bc6f9d63756bc90ff891ee1c3e743
belljun3395 <195850@jnu.ac.kr>
2024-12-20 23:15:35 +0900

    feat: LocalMessage 환경 구현

---
+++ event/src/main/kotlin/event/message/local/LocalMessageBroadCaster.kt
@@ -0,0 +1,50 @@
+package event.message.local
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import event.message.Message
+import event.message.MessagePayload
+import event.message.MessageReverseRelay
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import java.lang.reflect.Method
+
+fun Method.isForLocalMessageReverseRelay(): Boolean = this.annotations.any { annotation -> annotation is LocalSubscribeMessage }
+
+fun Method.messageType(): Class<*> = this.parameterTypes[0]
+
+@Suppress("LABEL_NAME_CLASH")
+open class LocalMessageBroadCaster(
+    private val objectMapper: ObjectMapper,
+    private val messageReverseRelays: List<MessageReverseRelay<*>>,
+) {
+    private val log = KotlinLogging.logger { }
+
+    @Async
+    @EventListener
+    open fun onApplicationEvent(message: Message) {
+        log.info { "[${Thread.currentThread().name}] Receive message: $message" }
+        messageReverseRelays.forEach { relay ->
+            relay.javaClass.methods
+                .filter { it.name.equals("onApplicationEvent") && it.isForLocalMessageReverseRelay() }
+                .forEach { method ->
+                    val messageType = method.messageType()
+                    method.getAnnotation(LocalSubscribeMessage::class.java).topic.let {
+                        if (!method.name.contains(it)) {
+                            return@forEach
+                        }
+                    }
+                    objectMapper.writeValueAsString(message.payload).let { payload ->
+                        objectMapper.readValue(payload, MessagePayload::class.java)?.let { messagePayload ->
+                            objectMapper.convertValue(messagePayload, messageType).let { relayMessage ->
+                                log.info {
+                                    "[${Thread.currentThread().name}] Publish message to ${relay.javaClass.simpleName}: $relayMessage"
+                                }
+                                method.invoke(relay, relayMessage)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}

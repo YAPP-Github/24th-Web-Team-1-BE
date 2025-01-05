fbb76c69501b852ec899568dad4f8398c36c5aa8
belljun3395 <195850@jnu.ac.kr>
2025-01-05 20:05:18 +0900

    refactor: localMessageBroadCaster 생성 코드 임시로 주석처리

--- library/event/src/main/kotlin/event/message/local/LocalMessageConfig.kt
+++ library/event/src/main/kotlin/event/message/local/LocalMessageConfig.kt
@@ -2,9 +2,7 @@ package event.message.local
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import event.message.MessageReverseRelay
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -22,27 +20,27 @@ class LocalMessageConfig {
             registerKotlinModule()
         }
 
-    @Bean
-    fun localMessageBeanPostProcessor(
-        objectMapper: ObjectMapper,
-        messageReverseRelays: List<MessageReverseRelay<*>>,
-    ): BeanFactoryPostProcessor =
-        BeanFactoryPostProcessor { beanFactory ->
-            beanFactory.beanDefinitionNames
-                .filter {
-                    it.contains("Local", ignoreCase = true) &&
-                        it.contains(
-                            "MessageReverseRelay",
-                            ignoreCase = true,
-                        )
-                }.takeIf { it.isNotEmpty() }
-                ?.also { localReverseBeanNames ->
-                    log.info { "LocalReverse Beans are found: ${localReverseBeanNames.joinToString(", ")}" }
-                    beanFactory.registerSingleton(
-                        "localMessageBroadCaster",
-                        LocalMessageBroadCaster(objectMapper, messageReverseRelays),
-                    )
-                    log.info { "Local message broadcaster is registered" }
-                }
-        }
+//    @Bean
+//    fun localMessageBeanPostProcessor(
+//        objectMapper: ObjectMapper,
+//        messageReverseRelays: List<MessageReverseRelay<*>>,
+//    ): BeanFactoryPostProcessor =
+//        BeanFactoryPostProcessor { beanFactory ->
+//            beanFactory.beanDefinitionNames
+//                .filter {
+//                    it.contains("Local", ignoreCase = true) &&
+//                        it.contains(
+//                            "MessageReverseRelay",
+//                            ignoreCase = true,
+//                        )
+//                }.takeIf { it.isNotEmpty() }
+//                ?.also { localReverseBeanNames ->
+//                    log.info { "LocalReverse Beans are found: ${localReverseBeanNames.joinToString(", ")}" }
+//                    beanFactory.registerSingleton(
+//                        "localMessageBroadCaster",
+//                        LocalMessageBroadCaster(objectMapper, messageReverseRelays),
+//                    )
+//                    log.info { "Local message broadcaster is registered" }
+//                }
+//        }
 }

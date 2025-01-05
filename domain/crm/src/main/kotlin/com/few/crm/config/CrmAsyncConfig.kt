37d4bfd5ed6ad550a8bfc5c763421791350695e8
belljun3395 <195850@jnu.ac.kr>
2025-01-05 19:24:47 +0900

    feat: CrmAsyncConfig 추가

---
+++ domain/crm/src/main/kotlin/com/few/crm/config/CrmAsyncConfig.kt
@@ -0,0 +1,8 @@
+package com.few.crm.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class CrmAsyncConfig
